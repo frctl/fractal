b1a93e006b8565e8d9ff11d469da21736754f30b
Mark Perkins <mark@allmarkedup.com>
2015-08-04 14:05:31 +0100

    Start on tree building

--- fractal.js
+++ fractal.js
@@ -1,5 +1,6 @@
 var merge = require('deepmerge');
 var Tree = require('./src/tree.js');
+var _ = require('lodash');
 
 module.exports = new Fractal;
 
@@ -13,14 +14,41 @@ function Fractal(){
         build:      null,
         tasks:      [],
     };
+    this.treeKeys = ['assets','components','views','docs'];
+    this.trees = {};
 };
 
 Fractal.prototype.configure = function(config){
-    this.config = merge(this.config, config);
+    this.config = this.expandConfig(merge(this.config, config));  
 };
 
 Fractal.prototype.run = function(){
-    var fileTree = new Tree(this.config);
-    var service = (typeof process.argv[2] === 'undefined' || process.argv[2] == 'server') ? 'server' : 'export';
-    return require('./src/services/' +  service + '.js')(this.config, fileTree);
+    var self = this;
+    this.treeKeys.forEach(function(treeKey){
+        if (self.config[treeKey]) {
+            self.trees[treeKey] = new Tree(self.config[treeKey]);
+        }
+        self.trees[treeKey] = null;
+    });
+
+    // var fileTree = new Tree(this.config);
+    // return this.getService(process.argv[2], this.config, fileTree);
 };
+
+Fractal.prototype.expandConfig = function(config){
+    var self = this;
+    return _.mapValues(config, function(item, key){
+        if (_.includes(self.treeKeys, key) && ! _.isObject(item)) {
+            return {
+                dir: item
+            };
+        }
+        return item;
+    });
+};
+
+Fractal.prototype.getService = function(serviceName, config, fileTree){
+    var service = (typeof serviceName === 'undefined' || serviceName == 'server') ? 'server' : 'export';
+    return require('./src/services/' +  service + '.js')(config, fileTree);
+};
+
--- src/tree.js
+++ src/tree.js
@@ -1,7 +1,76 @@
+var path = require('path');
+var promise = require("bluebird");
+var fs = promise.promisifyAll(require("fs"));
+var _ = require('lodash');
+var matter = require('gray-matter');
+var absolute = require('absolute');
+var merge = require('deepmerge');
+
 module.exports = Tree;
 
 function Tree(config){
     if (!(this instanceof Tree)) return new Tree(config);
     this.config = config;
-    console.log('Building tree...');
+    this.tree = null;
+};
+
+Tree.prototype.build = function(){
+    this.tree = this.readDir(this.config.dir);
+    this.tree.then(function(item){
+        console.log(item);
+    });
+};
+
+Tree.prototype.refresh = function(){
+    // TODO: add tree refresh functionality
+};
+
+/**
+ * Read all the files from the source dir
+ */
+
+Tree.prototype.readDir = function(dir){
+    var self = this;
+    var parseDir = function(dirName) {
+        return fs.readdirAsync(dirName).map(function (fileName) {
+            var filePath = path.join(dirName, fileName);
+            return fs.statAsync(filePath).then(function(stat) {
+                return stat.isDirectory() ? parseDir(filePath) : self.readFile(filePath, stat);
+            });
+        }).reduce(function (a, b) {
+            return a.concat(b);
+        }, []);
+    };
+    return parseDir(dir);
+};
+
+/**
+ * Build a representation of the file from the filesystem,
+ * including parsing any frontmatter if it exists.
+ */
+
+Tree.prototype.readFile = function(file, stat){
+    
+     var self = this;
+
+    return fs.readFileAsync(file).then(function(buffer) {
+        var item = {
+            rootPath: file.replace(new RegExp('^(' + process.cwd() + '\.)'),""),
+            absPath: path.resolve(file),
+            relPath: file.replace(new RegExp('^(' + self.config.dir + '\.)'),"")
+        };
+        var parsed = matter(buffer.toString());
+        var fileInfo = path.parse(file);
+        var previewData = parsed.data.preview || {};
+        delete parsed.data.preview;
+
+        item.content = parsed.content.trim() + "\n";
+        item.meta = parsed.data;
+        item.data = previewData;
+        item = merge(item, fileInfo);
+        item.modified = stat.mtime;
+        item.dir = item.dir.replace(new RegExp('^(' + self.config.dir + '\.)'),"");
+
+        return item; 
+    });
 };
