0b541fbe6a69c382bcca2f6f239efde71d637c42
Mark Perkins <mark@allmarkedup.com>
2015-10-02 12:18:36 +0100

    More work on color swatch generator

--- src/fs/pipes/generators.js
+++ src/fs/pipes/generators.js
@@ -1,19 +1,32 @@
 var Handlebars  = require('handlebars');
 var _           = require('lodash');
+var tinycolor   = require("tinycolor2");
+var path        = require("path");
+var fs          = require("fs");
 
-var hbs = Handlebars.create();
+var config      = require("../../config");
+var hbs         = Handlebars.create();
 
 module.exports = Generators;
 
+var swatchesTemplate = hbs.compile(fs.readFileSync(path.join(config.get('theme.views'),'generators/swatches.hbs'), 'utf8'));
 hbs.registerHelper('swatches', function(colours) {
     var output = '';
-    colours = _.compact(_.map(colours.split('|'), function(colour){
-        return _.trim(colour);
-    }));
-    _.each(colours, function(colour){
-        output = output + '<span style="display: inline-block; background-color: ' + colour + '; height: 100px; width: 100px;"></span>';
+    if (_.isString(colours)) {
+        colours = _.compact(_.map(colours.split('|'), function(colour){
+            return {
+                label: null,
+                color: _.trim(colour)
+            };
+        }));
+    }
+    colours = _.map(colours, function(col){
+        var c = tinycolor(col.color);
+        col.label = col.label || col.color;
+        col.textColor = c.isDark() ? 'white' : 'black';
+        return col;
     });
-    return new Handlebars.SafeString('<div>' + output + '</div>');
+    return new Handlebars.SafeString(swatchesTemplate({colors: colours}));
 });
 
 function Generators(){
@@ -21,13 +34,13 @@ function Generators(){
 
 Generators.prototype.process = function(item){
     var template = hbs.compile(item.raw.toString());
-    item.raw = new Buffer(template(this.getData()) + "\n", "utf-8");
+    item.raw = new Buffer(template(this.getData(item)) + "\n", "utf-8");
     return item;
 };
 
-Generators.prototype.getData = function(){
-    // TODO
+Generators.prototype.getData = function(item){
     return {
-
+        page: item.data,
+        config: config.all()
     };
 };
