23a57c859deef68a5f25f7dc151cfd6cce6013eb
Mark Perkins <mark@allmarkedup.com>
2016-01-11 16:26:19 +0000

    Cleanup component generator

--- src/services/generator/handlers/component.js
+++ src/services/generator/handlers/component.js
@@ -2,17 +2,9 @@
  * Module dependencies.
  */
 
-var logger      = require('winston');
-var Promise     = require('bluebird');
 var path        = require('path');
-var _           = require('lodash');
-var fs          = Promise.promisifyAll(require('fs'));
-var mkdirp      = Promise.promisify(require('mkdirp'));
-var ncp         = Promise.promisify(require('ncp'));
-var rimraf      = Promise.promisify(require('rimraf'));
 
 var ExistsError = require('../../../errors/exists');
-var utils = require('../../../utils');
 
 
 module.exports = ComponentGenerator;
@@ -46,55 +38,5 @@ ComponentGenerator.prototype.generate = function(relPath, opts){
         });
     }).then(function(components){
         return components.create(relPath)
-        // var pathParts = path.parse(fullPath);
-        // var title = utils.titlize(pathParts.name);
-        //
-        // var config = {
-        //     handle: pathParts.name,
-        //     label: utils.titlize(pathParts.name)
-        //     context: {}
-        // };
-        //
-        // var templatePath = path.join(fullPath, pathParts.name + self.app.getComponentViewEngine().ext);
-        // var configPath = path.join(fullPath, self.app.get('generator:config:name').replace('{{name}}', pathParts.name));
-        //
-        // var writes = [
-        //     fs.writeFileAsync(templatePath, '<p>' +  + ' component</p>'),
-        //     fs.writeFileAsync(configPath, JSON.stringify(config, null, 4))
-        // ];
-        //
-        // return Promise.all(writes);
     });
 };
-
-// /**
-//  * Export the generator function
-//  */
-//
-// module.exports = function(argv, app){
-//
-//     var type = argv._[1];
-//     var relPath = argv._[2];
-//
-//     switch (type) {
-//         case 'component':
-//             var rootPath = app.get('components:path');
-//             break;
-//         case 'page':
-//             var rootPath = app.get('pages:path');
-//             break;
-//         default:
-//             logger.error("Entity type '%s' is not recognised. Try 'page' or 'component' instead.", type);
-//             process.exit(1);
-//             return;
-//     }
-//
-//     var components = app.getComponents();
-//     .findByPath(relPath.trim('/'));
-//
-//     // var entityPath = path.join(rootPath, relPath);
-//
-//     console.log(entityPath);
-//
-//     process.exit(0);
-// };
