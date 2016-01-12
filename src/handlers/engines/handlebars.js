497fe66253d6bae066d2f90e5d4e31b559d7018e
Mark Perkins <mark@allmarkedup.com>
2016-01-12 12:11:40 +0000

    Add empty handlebars engine handler

---
+++ src/handlers/engines/handlebars.js
@@ -0,0 +1,20 @@
+/**
+ * Module dependencies.
+ */
+
+var path        = require('path');
+var _           = require('lodash');
+var consolidate = require('consolidate');
+var RenderError  = require('../errors/render');
+
+/*
+ * Export the component renderer.
+ */
+
+module.exports = {
+
+    render: function(variant, ){
+
+    }
+
+};
