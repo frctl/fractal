18c73efa62c0d4bd74a67f2a1b15eb73aab7ac70
Mark Perkins <mark@allmarkedup.com>
2016-01-14 15:02:15 +0000

    Make missing assets 404 correctly

--- src/services/server/handlers/pages.js
+++ src/services/server/handlers/pages.js
@@ -6,6 +6,7 @@ var Promise         = require('bluebird');
 var path            = require('path');
 var _               = require('lodash');
 
+var highlighter     = require('../../../highlighter');
 var utils           = require('../../../utils');
 
 /*
@@ -20,11 +21,17 @@ handlers.params = {};
  */
 
 handlers.params.page = function(req, res, next, pagePath) {
-    var page = req._pages.resolve(pagePath);
-    page.renderContent().then(function(){
-        res.locals.page = page.toJSON();
-        next();
-    });
+    try {
+        var page = req._pages.resolve(pagePath);
+        page.renderContent().then(function(){
+            res.locals.page = page.toJSON();
+            next();
+        });
+    } catch(err) {
+        return res.status(404).render('pages/404', {
+            message: err.message
+        });
+    }
 };
 
 /*
