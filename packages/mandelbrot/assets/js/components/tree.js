e4da14e99251df0b663ad8828df3cdf731d9ff60
Mark Perkins <mark@allmarkedup.com>
2016-02-25 17:55:52 +0000

    Working on sidebar

---
+++ packages/mandelbrot/assets/js/components/tree.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const $       = global.jQuery;
+const storage = require('../storage');
+
+class Tree {
+
+    constructor(el){
+        this._el = $(el);
+        this._id = this._el[0].id;
+        this._state = storage.get(`tree.${this._id}.state`, []);
+        console.log(this._state);
+        this._collections = $.map(this._el.find('[data-behaviour="collection"]'), c => new TreeCollection(c, this));
+        for (let collection of this._collections) {
+            if (collection.containsCurrentItem()) {
+                this._state.push(collection.id);
+            }
+        }
+        this._state = jQuery.unique(this._state);
+        this.applyState();
+    }
+
+    applyState() {
+        for (let collection of this._collections) {
+            if (this._state.includes(collection.id)) {
+                collection.open(true);
+            } else {
+                collection.close(true);
+            }
+        }
+    }
+
+    saveState() {
+        this._state = this._collections.filter(c => c.isOpen).map(c => c.id);
+        storage.set(`tree.${this._id}.state`, this._state);
+    }
+
+}
+
+class TreeCollection {
+
+    constructor(el, tree){
+        this._tree         = tree;
+        this._el           = $(el);
+        this._toggle       = this._el.find('> [data-role="toggle"]');
+        this._itemsWrapper = this._el.find('[data-role="items"]:not(> [data-behaviour] [data-role="items"])');
+        this._isOpen       = true;
+        this._toggle.on('click', this.toggle.bind(this));
+    }
+
+    get id(){
+        return this._el[0].id;
+    }
+
+    get isOpen(){
+        return this._isOpen;
+    }
+
+    containsCurrentItem(){
+        return !! this._itemsWrapper.find('[data-state="current"]').length;
+    }
+
+    open(silent){
+        this._el.removeClass('is-closed');
+        this._isOpen = true;
+        if (!silent) this._tree.saveState();
+    }
+
+    close(silent){
+        this._el.addClass('is-closed');
+        this._isOpen = false;
+        if (!silent) this._tree.saveState();
+    }
+
+    toggle(){
+        this._isOpen ? this.close() : this.open();
+        return false;
+    }
+}
+
+module.exports = Tree;
