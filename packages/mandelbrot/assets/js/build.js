92de768ccbdfe934c69dc73f862dbb6bf437bf69
Mark Perkins <mark@allmarkedup.com>
2016-02-26 16:20:14 +0000

    Add pjax support

--- packages/mandelbrot/assets/js/build.js
+++ packages/mandelbrot/assets/js/build.js
@@ -4,6 +4,7 @@ global.jQuery    = require('jquery');
 const pjax       = require('jquery-pjax');
 const fastclick  = require('fastclick');
 const $          = global.jQuery;
+const doc        = $(document);
 
 const events     = require('./events');
 const frame      = require('./components/frame');
@@ -13,3 +14,11 @@ fastclick(document.body);
 
 const mainFrame = frame($('#frame'));
 const navTrees  = $.map($('[data-behaviour="tree"]'), t => new Tree(t));
+
+doc.pjax('a[data-pjax]', '#pjax-container', {
+    fragment: '#pjax-container'
+}).on('pjax:start', function(e, xhr, options){
+    events.trigger('main-content-preload', options.url);
+}).on('pjax:end', function(){
+    events.trigger('main-content-loaded');
+});
