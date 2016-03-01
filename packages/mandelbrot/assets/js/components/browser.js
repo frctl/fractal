cb8da8b94663f4f0146fcac563958847ccc3cacb
Mark Perkins <mark@allmarkedup.com>
2016-03-01 18:48:38 +0000

    Store tabs state

--- packages/mandelbrot/assets/js/components/browser.js
+++ packages/mandelbrot/assets/js/components/browser.js
@@ -9,20 +9,26 @@ class Browser {
     constructor(el){
         this._el   = $(el);
         this._tabs = this._el.find('[data-role="tab"]');
-        console.log(this._tabs);
         this._tabPanels = this._el.find('[data-role="tab-panel"]');
         this._initTabs();
     }
 
     _initTabs() {
+        const selectedIndex = Math.min(this._tabs.length -1, storage.get(`browser.selectedTabIndex`, 0));
         this._tabs.on('click', e => {
+            const link = $(e.target);
+            const tab = link.parent();
             this._tabs.removeClass('is-active');
-            $(e.target).parent().addClass('is-active');
+            storage.set(`browser.selectedTabIndex`, this._tabs.index(tab));
+            tab.addClass('is-active');
             this._tabPanels.removeClass('is-active');
-            this._tabPanels.filter($(e.target).attr('href')).addClass('is-active');
+            this._tabPanels.filter(link.attr('href')).addClass('is-active');
             return false;
         });
+        this._tabs.removeClass('is-active');
+        this._tabs.eq(selectedIndex).find('a').trigger('click');
     }
+
 }
 
 module.exports = Browser;
