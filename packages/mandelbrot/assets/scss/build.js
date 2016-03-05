c995c93f12fa12f48031762b5e282790b2f5ccb3
Mark Perkins <mark@allmarkedup.com>
2016-03-05 20:54:29 +0000

    Add extra colour schemes from mrmrs/colors

--- packages/mandelbrot/assets/scss/build.js
+++ packages/mandelbrot/assets/scss/build.js
@@ -7,35 +7,62 @@ const schemes = [{
     accent: '#0074d9',
     palette: 'cool'
 },{
+    name: 'aqua',
+    accent: '#7fdbff',
+    palette: 'cool'
+},{
     name: 'blue',
     accent: '#0074d9',
     palette: 'cool'
 },{
-    name: 'aqua',
-    accent: '#7fdbff',
+    name: 'lime',
+    accent: '#01ff70',
+    palette: 'cool'
+},{
+    name: 'navy',
+    accent: '#001f3f',
+    palette: 'cool'
+},{
+    name: 'teal',
+    accent: '#39cccc',
+    palette: 'cool'
+},{
+    name: 'olive',
+    accent: '#3d9970',
+    palette: 'cool'
+},{
+    name: 'green',
+    accent: '#2ecc40',
+    palette: 'cool'
+},{
+    name: 'red',
+    accent: '#ff4136',
+    palette: 'cool'
+},{
+    name: 'maroon',
+    accent: '#85144b',
+    palette: 'cool'
+},{
+    name: 'orange',
+    accent: '#ff851b',
+    palette: 'cool'
+},{
+    name: 'purple',
+    accent: '#b10dc9',
+    palette: 'cool'
+},{
+    name: 'yellow',
+    accent: '#ffdc00',
+    palette: 'cool'
+},{
+    name: 'fuchsia',
+    accent: '#f012be',
+    palette: 'cool'
+},{
+    name: 'grey',
+    accent: '#aaaaaa',
     palette: 'cool'
 }];
- //
- //
- // {
- //    aqua:    '#7fdbff',
- //    blue:    '#0074d9',
- //    lime:    '#01ff70',
- //    navy:    '#001f3f',
- //    teal:    '#39cccc',
- //    olive:   '#3d9970',
- //    green:   '#2ecc40',
- //    red:     '#ff4136',
- //    maroon:  '#85144b',
- //    orange:  '#ff851b',
- //    purple:  '#b10dc9',
- //    yellow:  '#ffdc00',
- //    fuchsia: '#f012be',
- //    gray:    '#aaaaaa',
- //    white:   '#ffffff',
- //    black:   '#111111',
- //    silver:  '#dddddd'
- // };
 
  for (let scheme of schemes) {
      fs.writeFile(`./assets/scss/schemes/${scheme.name}.scss`,
