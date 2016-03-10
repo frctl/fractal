63e04054c81a1aa1a7cd74c990c6294b00125223
Mark Perkins <mark@allmarkedup.com>
2016-03-10 22:00:31 +0000

    Improve setting theme colours in templates

--- packages/mandelbrot/assets/scss/build.js
+++ packages/mandelbrot/assets/scss/build.js
@@ -1,72 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-
-const schemes = [{
-    name: 'default',
-    accent: '#07d',
-    palette: 'cool'
-},{
-    name: 'aqua',
-    accent: '#7df',
-    palette: 'cool'
-},{
-    name: 'black',
-    accent: '#111',
-    palette: 'cool'
-},{
-    name: 'blue',
-    accent: '#07d',
-    palette: 'cool'
-},{
-    name: 'fuchsia',
-    accent: '#e1b',
-    palette: 'cool'
-},{
-    name: 'green',
-    accent: '#3c4',
-    palette: 'cool'
-},{
-    name: 'grey',
-    accent: '#aaa',
-    palette: 'cool'
-},{
-    name: 'lime',
-    accent: '#0f7',
-    palette: 'cool'
-},{
-    name: 'maroon',
-    accent: '#815',
-    palette: 'cool'
-},{
-    name: 'navy',
-    accent: '#024',
-    palette: 'cool'
-},{
-    name: 'olive',
-    accent: '#497',
-    palette: 'cool'
-},{
-    name: 'orange',
-    accent: '#f82',
-    palette: 'cool'
-},{
-    name: 'purple',
-    accent: '#a1c',
-    palette: 'cool'
-},{
-    name: 'red',
-    accent: '#f43',
-    palette: 'cool'
-},{
-    name: 'teal',
-    accent: '#3cc',
-    palette: 'cool'
-},{
-    name: 'yellow',
-    accent: '#fd0',
-    palette: 'cool'
-}];
+const schemes = require('../schemes.json');
 
 for (let scheme of schemes) {
     fs.writeFile(`./assets/scss/schemes/${scheme.name}.scss`,
