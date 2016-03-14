706031e79f57fc58416465bf3c739a3fb16ba25a
Mark Perkins <mark@allmarkedup.com>
2016-03-14 16:40:46 +0000

    Enable double-clicking drag handles

--- packages/mandelbrot/assets/js/components/pen.js
+++ packages/mandelbrot/assets/js/components/pen.js
@@ -19,12 +19,34 @@ class Pen {
     }
 
     _init() {
-        
+
         const initialHeight = storage.get(`pen.previewHeight`, this._previewPanel.outerHeight());
         const preview       = new Preview(this._previewPanel);
         const browser       = new Browser(this._browser);
+        let state           = storage.get(`pen.previewState`, 'open');
+        let handleClicks    = 0;
+        let dblClick        = false;
 
-        this._previewPanel.outerHeight(initialHeight);
+        if (state === 'open') {
+            this._previewPanel.outerHeight(initialHeight);
+        } else {
+            this._previewPanel.css('height', '100%');
+        }
+
+        this._handle.on('mousedown', e => {
+            dblClick = false;
+            handleClicks++;
+
+            setTimeout(function() {
+                handleClicks = 0;
+            }, 400);
+
+            if (handleClicks === 2) {
+                dblClick = true;
+                handleClicks = 0;
+                return false;
+            }
+        });
 
         this._previewPanel.resizable({
             handleSelector: this._handle,
@@ -38,7 +60,33 @@ class Pen {
                 this._el.removeClass('is-resizing');
                 preview.enableEvents();
                 events.trigger('end-dragging');
-                storage.set(`pen.previewHeight`, this._previewPanel.outerHeight());
+                if (dblClick) {
+                    if (state === 'closed') {
+                        this._previewPanel.css('height', storage.get(`pen.onClosedHeight`, initialHeight));
+                        state = 'open';
+                        storage.set(`pen.previewState`, 'open');
+                    } else {
+                        storage.set(`pen.onClosedHeight`, this._previewPanel.outerHeight());
+                        this._previewPanel.css({
+                            'height': '100%',
+                            'transition': '.3s ease all'
+                        });
+                        state = 'closed';
+                        storage.set(`pen.previewState`, 'closed');
+                    }
+                } else {
+                    if (state !== 'closed') {
+                        storage.set(`pen.previewHeight`, this._previewPanel.outerHeight());
+                    } else {
+                        setTimeout(() => {
+                            if (!dblClick) {
+                                state = 'open';
+                                storage.set(`pen.previewState`, 'open');
+                                storage.set(`pen.previewHeight`, this._previewPanel.outerHeight());
+                            }
+                        }, 400);
+                    }
+                }
             }
         });
     }
