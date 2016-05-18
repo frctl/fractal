0c1fabbf34c582962dc884dab7a8ce61e5307c67
Mark Perkins <mark@allmarkedup.com>
2016-05-18 16:52:09 +0100

    re-implementing pages

--- packages/mandelbrot/index.js
+++ packages/mandelbrot/index.js
@@ -14,12 +14,13 @@ module.exports = function(options){
         stylesheet: null,
         contextFormat: 'json',
     });
+    config.stylesheet = `/theme/css/${config.skin || 'default'}.css`;
 
     const theme = new Theme(Path.join(__dirname, 'views'), config);
 
-    // theme.setErrorView('pages/error.nunj');
+    theme.setErrorView('pages/error.nunj');
 
-    // theme.addStatic(Path.join(__dirname, 'dist'), '/theme');
+    theme.addStatic(Path.join(__dirname, 'dist'), '/theme');
 
     theme.addRoute('/', {
         handle: 'overview',
@@ -37,21 +38,49 @@ module.exports = function(options){
     theme.addRoute('/components/preview/:handle', {
         handle: 'preview',
         view: 'pages/components/preview.nunj'
-    }, [{ handle:'button'}] );
+    });
 
+    theme.addRoute('/components/detail/:handle', {
+        handle: 'component',
+        view: 'pages/components/detail.nunj'
+    });
 
+    theme.addRoute('/docs/:path([^\?]+?)', {
+        handle: 'page',
+        view: 'pages/doc.nunj'
+    });
 
-    // theme.addRoute('/components/detail/:handle', {
-    //     handle: 'component',
-    //     view: 'pages/components/detail.nunj'
-    // }, [{handle:'button'}, {handle:'alert'}]);
+    theme.on('init', function(env, app){
+
+        env.engine.addFilter('url', function(item){
+            if (item.isDoc) {
+                if (!item.path) {
+                    return '/';
+                }
+                return theme.urlFromRoute('page', {path: item.path})
+            } else if (item.isComponent || item.isVariant) {
+                return theme.urlFromRoute('component', {handle: item.handle})
+            }
+            throw new Error(`Cannot generate URL for ${item}`);
+        });
+
+    });
+
     //
-    // theme.addRoute('/docs/:path([^\?]+?)', {
-    //     handle: 'page',
-    //     view: 'pages/page.nunj'
-    // }, function(){
-    //     return [{path:'/'}];
-    // });
+    // % set contextFormat = frctl.theme.get('contextFormat') | lower %}
+    //
+    //  {% if entity.isComponent and entity.isCollated %}
+    //     {% set viewContent = entity.variants().getCollatedContentSync() %}
+    //     {% set ctx = entity.variants().getCollatedContext() | async %}
+    // {% elif entity.isComponent %}
+    //     {% set viewContent = entity.variants().default().getContentSync() %}
+    //     {% set ctx = entity.variants().default().getResolvedContext() | async %}
+    // {% else %}
+    //     {% set viewContent = entity.getContentSync() %}
+    //     {% set ctx = entity.getResolvedContext() | async %}
+    // {% endif %}
+    //
+    // {% set contextString = ctx | format(contextFormat) %}
 
 
     // theme.onBuild(function(builder, app){
