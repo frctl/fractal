7a9e3ce25699ff2d36ad2e755698c29f9cbfffeb
Mark Perkins <mark@allmarkedup.com>
2016-05-19 09:37:05 +0100

    Allow async filter to catch errors

--- src/web/engine/filters/async.js
+++ src/web/engine/filters/async.js
@@ -7,7 +7,19 @@ module.exports = function(app, engine) {
     return {
         name: 'async',
         async: true,
-        filter: (p, cb) => Promise.resolve(p).then(result => cb(null, result)).catch(cb)
+        filter() {
+            let args = Array.from(arguments);
+            let cb = args.pop();
+            if (args.length == 1) {
+                Promise.resolve(args[0]).then(result => cb(null, result)).catch(cb);
+            } else {
+                Promise.resolve(args[0]).then(result => cb(null, result)).catch(e => {
+                    engine._engine.addGlobal(args[1], e);
+                    cb(null, false);
+                    return false;
+                });
+            }
+        }
     }
 
 };
