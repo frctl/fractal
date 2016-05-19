4c26900f1bfc5836959ccd0af9fb2abfdd1faaf9
Mark Perkins <mark@allmarkedup.com>
2016-05-19 15:47:41 +0100

    Add error filter

---
+++ src/web/engine/filters/is-error.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Promise = require('bluebird');
+
+module.exports = function(app, engine) {
+
+    return {
+        name: 'isError',
+        filter(item) {
+            return item instanceof Error;
+        }
+    }
+
+};
