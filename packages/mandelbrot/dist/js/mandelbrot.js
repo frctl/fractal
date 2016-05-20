33d4f03327e7103304659fa01b446b89336f656e
Mark Perkins <mark@allmarkedup.com>
2016-05-20 12:14:16 +0100

    Theme fixes

--- packages/mandelbrot/dist/js/mandelbrot.js
+++ packages/mandelbrot/dist/js/mandelbrot.js
@@ -666,7 +666,7 @@ var pens = [];
 loadPen();
 
 if (window.location.protocol.indexOf('file') !== 0) {
-    doc.pjax('a[data-pjax], code a[href]', '#pjax-container', {
+    doc.pjax('a[data-pjax], code a[href], .Prose a[href]', '#pjax-container', {
         fragment: '#pjax-container',
         timeout: 10000
     }).on('pjax:start', function (e, xhr, options) {
