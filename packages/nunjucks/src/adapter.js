ae1a58791e65afb6afcc96144cc609a6794907a2
Mark Perkins <mark@allmarkedup.com>
2016-05-24 15:02:59 +0100

    Update to use new Adapter base class

--- packages/nunjucks/src/adapter.js
+++ packages/nunjucks/src/adapter.js
@@ -3,87 +3,78 @@
 const _        = require('lodash');
 const Promise = require('bluebird');
 const nunjucks = require('nunjucks');
+const Adapter     = require('@frctl/fractal').Adapter;
 
-module.exports = function(config){
+class NunjucksAdapter extends Adapter {
+
+    constructor(source, loadPaths) {
+
+        super(null, source);
+
+        const loaders = [];
+        const self = this;
+
+        /**
+         * Create a custom string loader and instantiate a new Nunjucks environment object with it.
+         * We don't want to use the FileSystemLoader as we already have
+         * the contents of all files cached in the component file tree.
+         */
+
+        const StringLoader = nunjucks.Loader.extend({
+            getSource: function(handle) {
+                if (handle.indexOf('@') !== 0) {
+                    return;
+                }
+                const view = self.getView(handle);
+                if (view) {
+                    return {
+                        src: view.content,
+                        path: view.content,
+                        noCache: true
+                    };
+                }
+            }
+        });
+        loaders.push(new StringLoader());
+
+        /**
+         * If the user has specified any paths for directly loading templates,
+         *  include a FileSystemLoader instance.
+         */
+
+        if (loadPaths) {
+            nj.loaders.push(new nunjucks.FileSystemLoader(loadPaths, {
+                watch: true
+            }));
+        }
+
+        /**
+         * Instantiate the Nunjucks environment instance.
+         */
+
+        let nj = Promise.promisifyAll(new nunjucks.Environment(loaders, {
+            autoescape: false
+        }));
+
+        this._engine = nj;
+    }
+
+    render(path, str, context, meta) {
+        return this.engine.renderStringAsync(str, context);
+    }
+
+}
+
+module.exports = function(config) {
 
     config = config || {};
 
-    let viewCache = null;
-    const loaders = [];
-
-    /**
-     * Create a custom string loader and instantiate a new Nunjucks environment object with it.
-     * We don't want to use the FileSystemLoader as we already have
-     * the contents of all files cached in the component file tree.
-     */
-
-    const StringLoader = nunjucks.Loader.extend({
-        getSource: function(handle) {
-            if (handle.indexOf('@') !== 0) {
-                return;
-            }
-            handle = handle.replace('@','');
-            const view = _.find(viewCache, function(view){
-                return (view.handle === handle || view.alias === handle);
-            });
-            if (view) {
-                return {
-                    src: view.content,
-                    path: view.path,
-                    noCache: true
-                };
-            }
-        }
-    });
-    loaders.push(new StringLoader());
-
-    /**
-     * If the user has specified any paths for directly loading templates,
-     * also include a FileSystemLoader instance.
-     */
-
-    if (config.paths) {
-        loaders.push(new nunjucks.FileSystemLoader(config.paths, {
-            watch: true
-        }));
-    }
-
-    /**
-     * Now instantiate the Nunjucks environment instance
-     * and load any helpers etc.
-     */
-
-    let nj = new nunjucks.Environment(loaders, {
-        autoescape: false
-    });
-
-    nj = Promise.promisifyAll(nj);
-
-    _.each(config.filters || {}, function(filter, name){
-        addFilter(name, filter);
-    });
-    _.each(config.extensions || {}, function(ext, name){
-        nj.addExtension(name, ext);
-    });
-    _.each(config.globals || {}, function(value, name){
-        nj.addGlobal(name, value);
-    });
-
-    function addFilter(name, filter){
-        if (typeof filter === 'function') {
-            nj.addFilter(name, filter);
-        } else if (typeof filter === 'object') {
-            nj.addFilter(name, filter.filter, filter.async);
-        }
-    }
-
     return {
 
-        get engine(){
-            return nj;
-        },
-
         register(source, app) {
+
+            const adapter = new NunjucksAdapter(source, config.paths);
+            const nj = adapter.engine;
 
             if (!config.pristine) {
                 _.each(require('./filters')(app) || {}, function(filter, name){
@@ -94,21 +85,26 @@ module.exports = function(config){
                 });
             }
 
-            function loadViews() {
-                viewCache = source.flattenDeep().items();
+            _.each(config.filters || {}, function(filter, name){
+                addFilter(name, filter);
+            });
+            _.each(config.extensions || {}, function(ext, name){
+                nj.addExtension(name, ext);
+            });
+            _.each(config.globals || {}, function(value, name){
+                nj.addGlobal(name, value);
+            });
+
+            function addFilter(name, filter){
+                if (typeof filter === 'function') {
+                    nj.addFilter(name, filter);
+                } else if (typeof filter === 'object') {
+                    nj.addFilter(name, filter.filter, filter.async);
+                }
             }
 
-            source.on('loaded', () => loadViews());
-            source.on('changed', data => loadViews());
-
-            loadViews();
-        },
-
-        render(path, str, context, meta) {
-            return nj.renderStringAsync(str, context);
+            return adapter;
         }
-
     }
-
 
 };
