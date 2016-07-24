08d4831d93d9df5e1937ec81c66189e94e20566b
Mark Perkins <mark@allmarkedup.com>
2016-07-24 16:11:45 +0100

    Support variable interpolation in path strings

--- packages/handlebars/src/helpers/path.js
+++ packages/handlebars/src/helpers/path.js
@@ -6,8 +6,18 @@ const utils      = require('@frctl/fractal').utils;
 
 module.exports = function(fractal){
 
-    return function staticPath(path, root){
-        root = this._config ? this : root;
+    return function staticPath(path){
+
+        const options = Array.from(arguments).pop();
+        const root = options.data.root;
+
+        if (path.includes('{{')) {
+            let context = _.defaults({}, this, _.pickBy(root, (item, key) => key.startsWith('_')));
+            const tpl = Handlebars.compile(path, {
+                data: false
+            });
+            path = tpl(context);
+        }
 
         if (! root || ! root._env || root._env.server) {
             return path;
