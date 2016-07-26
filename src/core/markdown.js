a9751ef4033efb780519c01a474d3e729ef0beb9
Mark Perkins <mark@allmarkedup.com>
2016-07-26 21:34:49 +0100

    Prevent unintended escaping of code without a lang attribute

--- src/core/markdown.js
+++ src/core/markdown.js
@@ -3,23 +3,13 @@
 const marked      = require('marked');
 const _           = require('lodash');
 const highlighter = require('./highlighter');
+const renderer    = new marked.Renderer();
 
-marked.setOptions({
-    highlight: (code, lang) => highlighter(code, lang)
-});
-
-const renderer = new marked.Renderer();
 renderer.code = function (code, lang, escaped) {
-    var highlighted = false;
-    if (this.options.highlight) {
-        var out = this.options.highlight(code, lang);
-        if (out != null && out !== code) {
-            escaped = true;
-            code = out;
-            highlighted = true;
-        }
+    var output = highlighter(code, lang);
+    if (output != null) {
+        code = output;
     }
-    code = escaped ? code : escape(code, true);
     if (!lang) {
         return `<code><pre>${code}</pre></code>`;
     }
