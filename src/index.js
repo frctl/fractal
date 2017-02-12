0a90eb82e3de41fd499dbfb9c5743c0354c739c2
Mark Perkins <mark@allmarkedup.com>
2017-02-12 12:04:11 +0000

    Include render extension

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const fractal = require('@frctl/core');
 const fs = require('@frctl/fs');
 const extractArgs = require('extract-opts');
 const EventEmitter = require('eventemitter2').EventEmitter2;
+const renderExtension = require('@frctl/extension-render');
 const defaults = require('./defaults');
 
 module.exports = function(opts = {}){
@@ -11,9 +12,13 @@ module.exports = function(opts = {}){
 
   const compSrc = utils.normalizePaths(opts.components.src);
   const components = fractal(opts.components);
+
   const events = new EventEmitter({
     wildcard: true
   });
+
+  const renderer = components.extend(renderExtension(opts.render));
+  renderer.add('nunjucks'); // default template engine
 
   function watch(paths, callback){
     fs.watch(paths, (event, path) => {
