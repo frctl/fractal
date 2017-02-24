8e6524aacb3687a0fc37aca2a62d00f6e0354307
Mark Perkins <mark@allmarkedup.com>
2017-02-24 11:52:10 +0000

    Lint fixes and parser tests

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,8 +1,8 @@
-/* eslint no-unused-expressions : "off" */
+/* eslint no-unused-expressions : "off", handle-callback-err: "off", new-cap: "off" */
 
 const Parser = require('@frctl/internals').Parser;
 const Processor = require('@frctl/internals').Processor;
-const fs = require('@frctl/fs');
+const fs = require('@frctl/ffs');
 const expect = require('@frctl/utils/test').expect;
 const parser = require('../src/parser');
 
@@ -18,7 +18,7 @@ const files = [
   'collection/config.js',
   'collection/random.txt',
   'assets',
-  'assets/global.css',
+  'assets/global.css'
 ].map(path => fs.File({
   cwd, base,
   path: `${base}/${path}`,
@@ -39,7 +39,7 @@ describe('parser()', function () {
 
   it(`identifies components`, function (done) {
     const ps = parser();
-    ps.process(files, function(err, result){
+    ps.process(files, function (err, result) {
       expect(result.components.length).to.equal(2);
       done();
     });
@@ -47,23 +47,23 @@ describe('parser()', function () {
 
   it(`identifies collections`, function (done) {
     const ps = parser();
-    ps.process(files, function(err, result){
+    ps.process(files, function (err, result) {
       expect(result.collections.length).to.equal(1);
       done();
     });
   });
 
-  it(`identifies collections`, function (done) {
+  it(`identifies files`, function (done) {
     const ps = parser();
-    ps.process(files, function(err, result){
-      expect(result.collections.length).to.equal(1);
+    ps.process(files, function (err, result) {
+      expect(result.files.length).to.equal(5);
       done();
     });
   });
 
   it(`sets a '.role' property on all files`, function (done) {
     const ps = parser();
-    ps.process(files, function(err, result){
+    ps.process(files, function (err, result) {
       for (const file of result.files) {
         expect(file).to.have.property('role');
       }
@@ -73,12 +73,11 @@ describe('parser()', function () {
 
   it(`sets a '.scope' property on all files`, function (done) {
     const ps = parser();
-    ps.process(files, function(err, result){
+    ps.process(files, function (err, result) {
       for (const file of result.files) {
         expect(file).to.have.property('scope');
       }
       done();
     });
   });
-
 });
