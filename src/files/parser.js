b6a1fb6fbe05e44004c86cfab39acfd41df72619
Mark Perkins <mark@allmarkedup.com>
2017-03-10 12:11:49 +0000

    Expose files and component objects instead of parsers and api builders directly

--- src/files/parser.js
+++ src/files/parser.js
@@ -3,7 +3,7 @@ const config = require('@frctl/fractal-plugin-files-parse-config');
 const name = require('./plugins/name');
 const role = require('./plugins/role');
 
-module.exports = function (opts = {}) {
+module.exports = function (fractal, opts = {}) {
   const files = new Parser();
 
   files.use(name(opts.name))
