98a1182a6b4fc79e105f21d02d12b471d8b34fb4
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-08-01 14:05:40 +0100

    Add Utils internals package

---
+++ packages/internals/utils/src/utils.js
@@ -0,0 +1,160 @@
+/* eslint import/no-dynamic-require: "off" */
+const path = require('path');
+const _ = require('lodash');
+const slug = require('slugify');
+const assert = require('check-types').assert;
+const resolveDeep = require('deep-aplus')(Promise);
+const slash = require('slash');
+const pify = require('pify');
+
+const utils = module.exports = {
+
+  /*
+   * Objects
+   */
+
+  defaultsDeep(...args) {
+    let output = {};
+    args.reverse().map(item => _.cloneDeep(item)).forEach(item => {
+      _.mergeWith(output, item, (objectValue, sourceValue) => {
+        if (_.isArray(sourceValue)) {
+          return sourceValue;
+        }
+        if (!_.isPlainObject(sourceValue) || !_.isPlainObject(objectValue)) {
+          return sourceValue;
+        }
+        if (_.isUndefined(sourceValue)) {
+          return objectValue;
+        }
+      });
+    });
+    return output;
+  },
+
+  toJSON(item) {
+    if (item && typeof item.toJSON === 'function') {
+      return item.toJSON();
+    }
+    return item;
+  },
+
+  async resolveDeep(target, ...args) {
+    target = await Promise.resolve(target);
+    if (_.isPlainObject(target)) {
+      return resolveDeep(target);
+    }
+    if (typeof target === 'function') {
+      const result = await Promise.resolve(target(...args));
+      if (_.isPlainObject(result)) {
+        return resolveDeep(result);
+      }
+    }
+    throw new Error('Can only resolve objects or functions/promises that return an object');
+  },
+
+  /*
+   * Strings
+   */
+
+  slugify(str, replacement) {
+    return slug(str.toLowerCase(), replacement || '-');
+  },
+
+  titlize(str) {
+    return require('titlecase')(str.replace(/[-_]/g, ' '));
+  },
+
+  normalizeName(str) {
+    return _.kebabCase(str.replace(/^[^a-zA-Z]*/, ''));
+  },
+
+  uniqueName(name, used) {
+    assert.array(used, 'You must provide an array to use when checking names for uniqueness [no-used-array]');
+    let id = name;
+    if (used.includes(id)) {
+      // id already exists, add a suffix to make it unique
+      let suffix = 0;
+      while (used.includes(id)) {
+        suffix++;
+        id = `${name}-${suffix}`;
+      }
+    }
+    used.push(id);
+    return id;
+  },
+
+  hash(content) {
+    if (typeof content !== 'string') {
+      content = JSON.stringify(content);
+    }
+    return require('crypto').createHash('md5').update(content).digest('hex');
+  },
+
+  matches(paths, patterns, caseSensitive = false) {
+    const mm = require('multimatch');
+    const matches = mm(utils.toArray(paths), utils.toArray(patterns), {
+      nocase: !caseSensitive
+    });
+    return matches && matches.length > 0;
+  },
+
+  /*
+   * Paths
+   */
+
+  addTrailingSeparator(str) {
+    return path.join(str, path.sep);
+  },
+
+  removeTrailingSeparator(str) {
+    while (utils.endsInSeparator(str)) {
+      str = str.slice(0, -1);
+    }
+    return str;
+  },
+
+  endsInSeparator(str) {
+    const last = str[str.length - 1];
+    return str.length > 1 && (last === '/' || (utils.isWin() && last === '\\'));
+  },
+
+  normalizeExt(ext) {
+    return `.${ext.toLowerCase().replace(/^\./, '')}`;
+  },
+
+  normalizePath(filePath, cwd) {
+    assert.string(filePath, `Path must be a string. Received '${typeof filePath}' [paths-invalid]`);
+    cwd = cwd || process.cwd();
+
+    if (!path.isAbsolute(filePath)) {
+      filePath = path.join(cwd, filePath);
+    }
+    filePath = utils.removeTrailingSeparator(slash(filePath));
+    return path.normalize(filePath);
+  },
+
+  normalizePaths(paths, cwd) {
+    paths = utils.toArray(paths);
+    return paths.map(filePath => utils.normalizePath(filePath, cwd));
+  },
+
+  /*
+   * Other
+   */
+
+  promisify(fn) {
+    return pify(fn);
+  },
+
+  toArray(args) {
+    if (args === null || args === undefined) {
+      return [];
+    }
+    return [].concat(args);
+  },
+
+  isWin() {
+    return process.platform === 'win32';
+  }
+
+};
---
+++ packages/internals/utils/src/utils.test.js
@@ -0,0 +1,423 @@
+/* eslint no-unused-expressions: "off" */
+
+const path = require('path');
+const {
+  expect,
+  sinon
+} = require('../../../../test/helpers');
+const utils = require('../.');
+
+/*
+ * Support
+ */
+
+class MyClass {
+
+  constructor() {
+    this.foo = 'MyClass';
+  }
+
+}
+
+class MyOtherClass {
+
+  constructor() {
+    this.foo = 'MyOtherClass';
+  }
+
+}
+
+function timeoutPromiseFromObj(obj) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(obj);
+    }, 200);
+  });
+}
+/*
+ * Objects
+ */
+
+describe('.defaultsDeep()', function () {
+  it('Does not modify source objects', function () {
+    let target = {};
+    let defaults = {
+      foo: 'bar'
+    };
+    let result = utils.defaultsDeep(target, defaults);
+
+    expect(result).to.not.equal(defaults);
+    expect(result).to.not.equal(target);
+    expect(target).to.eql({});
+    expect(defaults).to.eql({
+      foo: 'bar'
+    });
+    expect(target).to.equal(target);
+    expect(defaults).to.equal(defaults);
+  });
+
+  it('Recursively merges plain objects', function () {
+    let target = {
+      top: 'from target',
+      item: {
+        nested: {
+          one: 'from target',
+          two: ['from', 'target'],
+          three: undefined,
+          four: {
+            five: 5
+          }
+        }
+      }
+    };
+    let defaults = {
+      item: {
+        def: 'from default',
+        nested: {
+          one: 'from default',
+          two: ['from', 'default'],
+          three: ['set', 'from', 'default'],
+          four: {
+            five: 9,
+            six: 6
+          }
+        }
+      }
+    };
+    let expected = {
+      top: 'from target',
+      item: {
+        def: 'from default',
+        nested: {
+          one: 'from target',
+          two: ['from', 'target'],
+          three: ['set', 'from', 'default'],
+          four: {
+            five: 5,
+            six: 6
+          }
+        }
+      }
+    };
+    expect(utils.defaultsDeep(target, defaults)).to.eql(expected);
+  });
+
+  it('Does not merge non-plain-object values', function () {
+    let target = {
+      item: (new MyClass())
+    };
+    let defaults = {
+      item: new MyOtherClass()
+    };
+    let plain = {
+      item: {
+        foo: 'plain'
+      }
+    };
+    expect(utils.defaultsDeep(target, defaults)).to.eql(target);
+    expect(utils.defaultsDeep(plain, defaults)).to.eql(plain);
+    expect(utils.defaultsDeep(target, plain)).to.eql(target);
+  });
+
+  it('Does not merge array values', function () {
+    let target = {
+      items: ['one', 'two']
+    };
+    let defaults = {
+      items: ['one', 'three', 'four']
+    };
+    expect(utils.defaultsDeep(target, defaults).items).to.eql(target.items);
+  });
+
+  it('Returns the default value if the target property is undefined', function () {
+    let target = {
+      anotherItem: 'foo',
+      nullItem: null,
+      undefinedItem: undefined
+    };
+    let defaults = {
+      item: ['one', 'three', 'four'],
+      nullItem: 'not null',
+      undefinedItem: 'not undefined'
+    };
+    let result = utils.defaultsDeep(target, defaults);
+    expect(result).to.have.property('anotherItem');
+    expect(result).to.have.property('item');
+    expect(result.nullItem).to.be.null;
+    expect(result.undefinedItem).to.equal('not undefined');
+  });
+});
+
+describe('.toJSON()', function () {
+  it('Does not convert non-JSONifiable arguments', function () {
+    expect(utils.toJSON('SOMETHING to JSONify')).to.equal('SOMETHING to JSONify');
+    expect(utils.toJSON(2)).to.equal(2);
+    expect(utils.toJSON({foo: 'bar'})).to.eql({foo: 'bar'});
+  });
+  it(`Converts an object with a 'toJSON' method to its JSON representation`, function () {
+    let a = {foo: 'bar', toJSON: function () {
+      return 'bar';
+    }};
+    console.log(a.toJSON());
+    expect(utils.toJSON(a)).to.equal('bar');
+  });
+});
+
+describe('.resolveDeep()', function () {
+  it('Throws an error if invalid arguments provided', async function () {
+    let e;
+    try {
+      await utils.resolveDeep('asd');
+    } catch (err) {
+      e = err;
+    }
+    expect(e.toString()).to.match(/Error/);
+  });
+
+  it('Resolves promises that return objects', async function () {
+    var x = await utils.resolveDeep(timeoutPromiseFromObj({
+      title: 'Hello World!'
+    }));
+    expect(x).to.eql({
+      title: 'Hello World!'
+    });
+  });
+  it('Resolves nested promises that return objects', async function () {
+    var x = await utils.resolveDeep(timeoutPromiseFromObj({
+      promise: timeoutPromiseFromObj({
+        title: 'Hello World!'
+      })
+    }));
+    expect(x).to.eql({
+      promise: {
+        title: 'Hello World!'
+      }
+    });
+  });
+  it('Resolves nested objects', async function () {
+    var x = await utils.resolveDeep({
+      title: 'Hello World!',
+      other: {
+        nested: 'value'
+      }
+    });
+    expect(x).to.eql({
+      title: 'Hello World!',
+      other: {
+        nested: 'value'
+      }
+    });
+  });
+  it('Resolves nested objects with promises', async function () {
+    var x = await utils.resolveDeep({
+      title: 'Hello World!',
+      promise: {
+        nested: timeoutPromiseFromObj({data: 'value'})
+      }
+    });
+    expect(x).to.eql({
+      title: 'Hello World!',
+      promise: {
+        nested: {data: 'value'}
+      }
+    });
+  });
+  it('Resolves nested objects with nested promises', async function () {
+    var x = await utils.resolveDeep({
+      title: 'Hello World!',
+      promise: {
+        nested: timeoutPromiseFromObj({data: 'value', deep: {deeper: timeoutPromiseFromObj({deepest: true})}})
+      }
+    });
+    expect(x).to.eql({
+      title: 'Hello World!',
+      promise: {
+        nested: {data: 'value', deep: {
+          deeper: {
+            deepest: true
+          }
+        }}
+      }
+    });
+  });
+  it('Resolves functions that return an object (single level only)', async function () {
+    var x = await utils.resolveDeep(function (value) {
+      return {data: value};
+    }, 'value!');
+
+    expect(x).to.eql({data: 'value!'});
+  });
+});
+/*
+ * Strings
+ */
+
+describe('.slugify()', function () {
+  it('Slugifys and lowercases a string', function () {
+    expect(utils.slugify('SOMETHING to Slugify')).to.equal('something-to-slugify');
+  });
+
+  it('Accepts an optional seperator', function () {
+    expect(utils.slugify('SOMETHING to Slugify', '_')).to.equal('something_to_slugify');
+  });
+});
+
+describe('.titlize()', function () {
+  it('Converts a string to title case', function () {
+    expect(utils.titlize('this is a STRING')).to.equal('This Is a STRING');
+  });
+
+  it('Replaces dashes and underscores with spaces', function () {
+    expect(utils.titlize('this-is-a-string')).to.equal('This Is a String');
+  });
+});
+
+describe('.normalizeName()', function () {
+  it('Removes leading non-alphanumeric characters from string', function () {
+    expect(utils.normalizeName('_string')).to.equal('string');
+    expect(utils.normalizeName('_01_string')).to.equal('string');
+    expect(utils.normalizeName('01_string')).to.equal('string');
+    expect(utils.normalizeName('__01_02_string')).to.equal('string');
+    expect(utils.normalizeName('_01-string')).to.equal('string');
+    expect(utils.normalizeName('01string')).to.equal('string');
+  });
+
+  it('Converts a string to kebab case', function () {
+    expect(utils.normalizeName('thisIsAString')).to.equal('this-is-a-string');
+    expect(utils.normalizeName('this.Is.A.String')).to.equal('this-is-a-string');
+    expect(utils.normalizeName('this.is.a.string')).to.equal('this-is-a-string');
+  });
+});
+
+describe('.uniqueName()', function () {
+  it('Checks that an array of existing values is provided', function () {
+    expect(function () {
+      utils.uniqueName('name');
+    }).to.throw(TypeError, '[no-used-array]');
+  });
+  it('Returns original name and adds it to used array, if unique', function () {
+    let name = 'name03';
+    let used = ['name00', 'name01', 'name02'];
+    let uniqueName = utils.uniqueName(name, used);
+    expect(uniqueName).to.equal(name);
+    expect(used).to.eql(['name00', 'name01', 'name02', 'name03']);
+  });
+  it('Returns modified unique name and adds it to used array, if not unique', function () {
+    let name = 'name02';
+    let used = ['name00', 'name01', 'name02'];
+    let uniqueName = utils.uniqueName(name, used);
+    expect(uniqueName).to.equal('name02-1');
+    let uniqueName2 = utils.uniqueName(name, used);
+    expect(uniqueName2).to.equal('name02-2');
+    expect(used).to.eql(['name00', 'name01', 'name02', 'name02-1', 'name02-2']);
+  });
+});
+
+describe('.hash()', function () {
+  it('Hashes a string', function () {
+    expect(utils.hash('foo')).to.be.a.string;
+    expect(utils.hash('foo').length).to.equal(32);
+    expect(utils.hash('foo')).to.equal(utils.hash('foo'));
+    expect(utils.hash('foo')).to.not.equal(utils.hash('bar'));
+  });
+  it('Hashes an object', function () {
+    let ob = {foo: 'bar'};
+    let ob2 = {foo: 'foo'};
+    expect(utils.hash(ob)).to.be.a.string;
+    expect(utils.hash(ob).length).to.equal(32);
+    expect(utils.hash(ob)).to.equal(utils.hash(ob));
+    expect(utils.hash(ob)).to.not.equal(utils.hash(ob2));
+  });
+});
+
+describe('.matches()', function () {
+  it('Matches path(s) against pattern(s)', function () {
+    expect(utils.matches('foo', 'foo')).to.equal(true);
+    expect(utils.matches(['foo'], ['foo'])).to.equal(true);
+    expect(utils.matches(['foo'], ['bar'])).to.equal(false);
+  });
+});
+
+/*
+ * Paths
+ */
+
+const pathWithEndSlash = path.join('this', 'is', 'a', 'path', '/');
+const pathWithoutEndSlash = path.join('this', 'is', 'a', 'path');
+
+describe('.addTrailingSeparator()', function () {
+  it('Appends a trailing path separator to the end of a string if it doesn\'t already have one', function () {
+    expect(utils.addTrailingSeparator(pathWithoutEndSlash)).to.equal(pathWithEndSlash);
+    expect(utils.addTrailingSeparator(pathWithEndSlash)).to.equal(pathWithEndSlash);
+  });
+});
+
+describe('.removeTrailingSeparator()', function () {
+  it('Removes a trailing path separator from the end of a string', function () {
+    expect(utils.removeTrailingSeparator(pathWithoutEndSlash)).to.equal(pathWithoutEndSlash);
+    expect(utils.removeTrailingSeparator(pathWithEndSlash)).to.equal(pathWithoutEndSlash);
+  });
+});
+
+describe('.endsInSeparator()', function () {
+  it('Test if a string ends in a path separator', function () {
+    expect(utils.endsInSeparator(pathWithoutEndSlash)).to.be.false;
+    expect(utils.endsInSeparator(pathWithEndSlash)).to.be.true;
+  });
+});
+
+describe('.normalizeExt()', function () {
+  it('Correctly format extension to be prefixed with a dot', function () {
+    expect(utils.normalizeExt('js')).to.equal('.js');
+    expect(utils.normalizeExt('.js')).to.equal('.js');
+  });
+});
+
+describe('.normalizePath()', function () {
+  it('Throws an error if non-string input is received', function () {
+    expect(function () {
+      utils.normalizePath(['one']);
+    }).to.throw(TypeError, '[paths-invalid]');
+  });
+  it('Normalises supplied absolute paths', function () {
+    expect(utils.normalizePath('/path/level')).to.equal('/path/level');
+    expect(utils.normalizePath('/path/level/')).to.equal('/path/level');
+    expect(utils.normalizePath('/path/level/', 'ignored/as/absolute')).to.equal('/path/level');
+  });
+  it('Normalises supplied relative paths', function () {
+    expect(utils.normalizePath('../path/level/')).to.equal(path.normalize(path.join(process.cwd(), '../path/level')));
+    expect(utils.normalizePath('../path/level/', 'cwd/of/project')).to.equal('cwd/of/path/level');
+  });
+});
+
+describe('.normalizePaths()', function () {
+  it(`Applies 'normalizePath' to each path in an array`, function () {
+    var spy = sinon.spy(utils, 'normalizePath');
+    utils.normalizePaths(['path/one', 'path/two', 'path/three']);
+    expect(spy.callCount).to.equal(3);
+  });
+});
+
+/*
+ * Other
+ */
+
+describe('.toArray()', function () {
+  it('Converts non-array values to an array', function () {
+    expect(utils.toArray('foo')).to.eql(['foo']);
+  });
+
+  it('Leaves existing arrays untouched', function () {
+    expect(utils.toArray(['foo', 'bar'])).to.eql(['foo', 'bar']);
+  });
+});
+
+describe('.promisify()', function () {
+  it('Converts a function to a Promise', function () {
+    function callback(data, cb) {
+      cb(null, data);
+    }
+    var pf = utils.promisify(callback);
+    expect(pf('value')).to.respondTo('then');
+  });
+});
