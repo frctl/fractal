df260e2dd0a216d8a0ee916455d44b8632310531
Mark Perkins <mark@allmarkedup.com>
2017-08-03 23:05:31 +0100

    Add schema-based config validation

--- packages/internals/config/src/config.js
+++ packages/internals/config/src/config.js
@@ -1,17 +1,23 @@
+const Ajv = require('ajv');
 const {assert} = require('check-types');
-const {get, set} = require('lodash');
+const {get, set, cloneDeep} = require('lodash');
 
-const _config = new WeakMap();
+const _data = new WeakMap();
+const _accessors = new WeakMap();
+const _validator = new WeakMap();
 
 class Config {
 
-  constructor(config = {}) {
-    assert.object(config, 'Only objects can be supplied to the Config object constructor [config-invalid]');
-    _config.set(this, config);
-  }
+  constructor(opts = {}) {
+    assert.object(opts, 'config opts must be an object [config-opts-invalid]');
 
-  get data() {
-    return _config.get(this);
+    _data.set(this, cloneDeep(opts.data || {}));
+    _accessors.set(this, opts.accessors || []);
+
+    const ajv = new Ajv();
+    _validator.set(this, ajv.compile(opts.schema || {}));
+
+    this.validate();
   }
 
   get(prop, fallback) {
@@ -21,7 +27,22 @@ class Config {
 
   set(prop, value) {
     assert.string(prop, 'Config.set - `prop` argument must be a string [prop-invalid]');
-    return set(this.data, prop, value);
+    set(this.data, prop, value);
+    this.validate();
+    return this;
+  }
+
+  validate() {
+    const validate = _validator.get(this);
+    if (!validate(this.data)) {
+      const errors = validate.errors.map(err => `'${err.dataPath.replace(/^\./,'')}' ${err.message}`);
+      throw new Error(`Configuration object validation failed with the following errors:\n${errors.join('\n')}` );
+    }
+    return this;
+  }
+
+  get data() {
+    return _data.get(this);
   }
 
 }
--- packages/internals/config/src/config.test.js
+++ packages/internals/config/src/config.test.js
@@ -3,16 +3,49 @@
 const {expect} = require('../../../../test/helpers');
 const Config = require('./config');
 
+const schema = {
+  properties: {
+    foo: {
+      type: 'number'
+    }
+  }
+};
+
 describe('Config', function () {
   describe('constructor', function () {
-    it('throws an error if a non-object config property is provided', function () {
-      expect(() => new Config('foo')).to.throw(TypeError, '[config-invalid]');
-      expect(() => new Config({prop: 'val'})).to.not.throw(TypeError);
+    it('throws an error if a non-object options argument is provided', function () {
+      expect(() => new Config('foo')).to.throw(TypeError, '[config-opts-invalid]');
+      expect(() => new Config({})).to.not.throw(TypeError);
     });
 
-    it('sets the data from the config argument', function () {
+    it('accepts an initial config data via opts.data', function () {
       const data = {prop: 'val'};
-      const config = new Config(data);
+      const config = new Config({data});
+      expect(config.data).to.eql(data);
+    });
+
+    it('clones provided data to prevent mutation', function () {
+      const data = {prop: 'val'};
+      const config = new Config({data});
+      expect(config.data).to.eql(data);
+      expect(config.data).to.not.equal(data);
+    });
+
+    it('validates input data against a schema, if supplied', function () {
+      expect(() => new Config({data: {foo: 123}, schema})).to.not.throw();
+      expect(() => new Config({data: {foo: '123'}, schema})).to.throw();
+    });
+  });
+
+  describe('.data', function () {
+    it('returns the full config data object', function () {
+      const data = {
+        one: 'two',
+        three: {
+          nested: 'four'
+        }
+      };
+      const config = new Config({data});
       expect(config.data).to.eql(data);
     });
   });
@@ -25,9 +58,10 @@ describe('Config', function () {
           nested: 'four'
         }
       };
-      const config = new Config(data);
+      const config = new Config({data});
       expect(config.get('three.nested')).to.eql(data.three.nested);
     });
+
     it('returns the supplied fallback argument if the property lookup returns undefined', function () {
       const config = new Config({});
       expect(config.get('does.not.exist')).to.equal(undefined);
@@ -37,10 +71,20 @@ describe('Config', function () {
 
   describe('.set()', function () {
     it('sets a config property via dot-notation syntax', function () {
-      const data = {};
-      const config = new Config(data);
-      config.set('test.this', 'foo');
-      expect(config.get('test.this')).to.equal('foo');
+      const config = new Config({});
+      config.set('foo.bar', 'baz');
+      expect(config.get('foo.bar')).to.equal('baz');
+    });
+
+    it('returns the config class instance', function () {
+      const config = new Config({});
+      expect(config.set('bar', 'foo')).to.equal(config);
+    });
+
+    it('throws an error if setting a value invalidates the config schema', function () {
+      const config = new Config({schema});
+      expect(() => config.set('foo', 123)).to.not.throw();
+      expect(() => config.set('foo', 'bar')).to.throw();
     });
   });
 });
