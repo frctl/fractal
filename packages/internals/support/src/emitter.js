1e3214e64fc14d6cdcee8cd90a4c1005ac443252
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-08-07 17:56:58 +0100

    Remove unused `log` method

--- packages/internals/support/src/emitter.js
+++ packages/internals/support/src/emitter.js
@@ -15,24 +15,6 @@ class Emitter extends EventEmitter2 {
   get namespace() {
     return _namespace.get(this);
   }
-
-  /**
-   * Convenience function for emitting a log events
-   *
-   * @param  {string} message The message string
-   * @param  {object|string} opts level string or options object
-   * @return {Fractal} The Fractal instance
-   */
-  log(message, opts = {}) {
-    opts = typeof opts === 'string' ? {level: opts} : opts;
-    const namespace = opts.namespace || _namespace.get(this);
-    opts.level = opts.level || 'debug';
-    if (namespace) {
-      opts.namespace = namespace;
-    }
-    this.emit(`log.${opts.level}`, message, opts);
-    return this;
-  }
 }
 
 module.exports = Emitter;
