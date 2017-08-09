a3bece9ef62605a06c8e4087a6bb0eb914047e08
Mark Perkins <mark@allmarkedup.com>
2017-08-09 23:46:10 +0100

    Add config store

--- packages/toolkit/fractal/index.test.js
+++ packages/toolkit/fractal/index.test.js
@@ -3,12 +3,14 @@ const {expect} = require('../../../test/helpers');
 const Fractal = require('./src/fractal');
 const factory = require('.');
 
-describe('main', () => {
-  it('exports a factory function that instantiates a Fractal instance', () => {
-    expect(factory()).to.be.instanceOf(Fractal);
-  });
+describe('Fractal', () => {
+  describe('main export', () => {
+    it('exports a factory function that instantiates a Fractal instance', () => {
+      expect(factory()).to.be.instanceOf(Fractal);
+    });
 
-  it('provides a reference to the Fractal class', () => {
-    expect(factory.Fractal).to.equal(Fractal);
+    it('provides a reference to the Fractal class', () => {
+      expect(factory.Fractal).to.equal(Fractal);
+    });
   });
 });
