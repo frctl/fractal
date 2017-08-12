7a0f9db2c69c53272f87a6097824436b3571fca1
Mark Perkins <mark@allmarkedup.com>
2017-08-12 14:15:50 +0100

    Add schema validation test helper

--- test/helpers.js
+++ test/helpers.js
@@ -1,7 +1,9 @@
 const Ajv = require('ajv');
+const ajvKeywords = require('ajv-keywords');
 const chai = require('chai');
 const chaiSorted = require('chai-sorted');
 const chaiAsPromised = require('chai-as-promised');
+const schemaSchema = require('./schema-schema');
 
 chai.use(chaiAsPromised);
 chai.use(chaiSorted);
@@ -13,7 +15,7 @@ module.exports.sinon = require('sinon');
 module.exports.mockRequire = require('mock-require');
 
 module.exports.validateSchema = function (testSchema) {
-  const schemaSchema = require('./schema-schema');
   const ajv = new Ajv();
+  ajvKeywords(ajv);
   return ajv.validate(schemaSchema, testSchema);
 };
