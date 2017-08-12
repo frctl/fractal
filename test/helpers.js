ceb53ca468ad04b210233cb2f296817b24e8bff7
Mark Perkins <mark@allmarkedup.com>
2017-08-12 15:36:55 +0100

    Tweak test schemas

--- test/helpers.js
+++ test/helpers.js
@@ -3,19 +3,24 @@ const ajvKeywords = require('ajv-keywords');
 const chai = require('chai');
 const chaiSorted = require('chai-sorted');
 const chaiAsPromised = require('chai-as-promised');
-const schemaSchema = require('./schema-schema');
+const schemaSchema = require('./schema/schema');
 
 chai.use(chaiAsPromised);
 chai.use(chaiSorted);
 
+function validate(schema, data) {
+  const ajv = new Ajv();
+  ajvKeywords(ajv, ['typeof', 'instanceof', 'regexp']);
+  return ajv.validate(schema, data);
+}
+
+function validateSchema(testSchema) {
+  return validate(schemaSchema, testSchema);
+}
+
 module.exports.expect = chai.expect;
-
 module.exports.sinon = require('sinon');
-
 module.exports.mockRequire = require('mock-require');
 
-module.exports.validateSchema = function (testSchema) {
-  const ajv = new Ajv();
-  ajvKeywords(ajv);
-  return ajv.validate(schemaSchema, testSchema);
-};
+module.exports.validate = validate;
+module.exports.validateSchema = validateSchema;
