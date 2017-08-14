5a8f1fe2a38614858c4760a72f63887e87f49f7b
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-08-14 17:50:00 +0100

    Begin Pipeline implementation

--- packages/internals/parser/src/transform/pipeline.js
+++ packages/internals/parser/src/transform/pipeline.js
@@ -1,6 +1,16 @@
-const {Emitter} = require('@frctl/support');
+// const debug = require('debug')('fractal:parser');
 
-class Pipeline extends Emitter {
+const _transforms = new WeakMap();
+
+class Pipeline {
+  constructor(transformers = []) {
+    _transforms.set(this, []);
+  }
+
+  get transforms() {
+    // _transforms.set(this, sortBy(_transforms.get(this), 'priority'));
+    return _transforms.get(this).slice(0);
+  }
 }
 
 module.exports = Pipeline;
--- packages/internals/parser/src/transform/pipeline.test.js
+++ packages/internals/parser/src/transform/pipeline.test.js
@@ -1,6 +1,6 @@
 /* eslint handle-callback-err: off, no-unused-expressions: off */
 
-const {Emitter} = require('@frctl/support');
+// const {Emitter} = require('@frctl/support');
 const {expect} = require('../../../../../test/helpers');
 const Pipeline = require('./pipeline');
 
@@ -9,7 +9,12 @@ describe('Pipeline', function () {
     it('returns a new instance if correct properties provided', function () {
       const pipeline = new Pipeline();
       expect(pipeline instanceof Pipeline).to.be.true;
-      expect(pipeline instanceof Emitter).to.be.true;
+    });
+  });
+  describe('.transforms', function () {
+    it(`has a 'transforms' getter`, function () {
+      const pipeline = new Pipeline();
+      expect(pipeline.transforms).to.eql([]);
     });
   });
 });
