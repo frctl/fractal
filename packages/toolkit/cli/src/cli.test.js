4661760b37200802472e994db3d4f8a26cb149f3
Mark Perkins <mark@allmarkedup.com>
2017-08-18 16:14:18 +0100

    Add placeholder test files

---
+++ packages/toolkit/cli/src/cli.test.js
@@ -0,0 +1,8 @@
+const {expect} = require('../../../../test/helpers');
+const Cli = require('./cli');
+
+describe('Cli', function () {
+  describe('constructor', function () {
+
+  });
+});
---
+++ packages/toolkit/cli/src/load-extension.test.js
@@ -0,0 +1,6 @@
+const {expect} = require('../../../../test/helpers');
+const loader = require('./load-extension');
+
+describe('Cli - load-extension', function () {
+
+});
