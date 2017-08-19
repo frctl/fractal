590b53470a8e80d2c75827c91319223719744520
Mark Perkins <mark@allmarkedup.com>
2017-08-19 01:01:07 +0100

    Fix tests

--- packages/toolkit/cli/src/cli.test.js
+++ packages/toolkit/cli/src/cli.test.js
@@ -1,8 +1,14 @@
+const command = require('../../../../test/fixtures/add-ons/command');
 const {expect} = require('../../../../test/helpers');
 const Cli = require('./cli');
 
 describe('Cli', function () {
   describe('constructor', function () {
-
+    it('add commands from the config', function () {
+      const cli = new Cli({
+        commands: [command]
+      });
+      expect(cli.getCommands().length).to.equal(1);
+    });
   });
 });
