dcb8b0d984a73a01cabe4610f49330701c6845b1
Mark Perkins <mark@allmarkedup.com>
2017-08-21 12:07:48 +0100

    Fix preset test

--- packages/cli/cli-preset-core/src/preset.test.js
+++ packages/cli/cli-preset-core/src/preset.test.js
@@ -1,9 +1,9 @@
-const {Preset} = require('@frctl/support');
+const schema = require('@frctl/support/schema');
 const {expect, validate} = require('../../../../test/helpers');
 const preset = require('./preset');
 
 describe('preset-core', function () {
   it('has the expected format', function () {
-    expect(validate(Preset.schema, preset())).to.equal(true);
+    expect(validate(schema.preset, preset())).to.equal(true);
   });
 });
