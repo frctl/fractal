cf5c7ff6c6b1d11216700704799f731173cf7edd
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-08-22 15:22:23 +0100

    Add fractal-plugin-files-name plugin
    # Set up plugin test runner

---
+++ packages/fractal/fractal-plugin-files-name/src/index.test.js
@@ -0,0 +1,24 @@
+const {expect} = require('../../../../test/helpers');
+const tests = require('../../../../test/runners/plugins')(__dirname);
+
+const input = [{
+  stem: '_foo'
+}, {
+  stem: '01-foo'
+}, {
+  stem: '_01-foo'
+}, {
+  stem: 'foo'
+}];
+
+tests.addPluginTest({
+  description: 'sets a name property on each item',
+  input: input,
+  test: function (result) {
+    for (const item of result) {
+      expect(item.name).to.equal('foo');
+    }
+  }
+});
+
+tests.run();
