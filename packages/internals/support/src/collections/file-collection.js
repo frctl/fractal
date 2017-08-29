1e0cff18493df53239e2760ca00b76fdb794c305
Mark Perkins <mark@allmarkedup.com>
2017-08-29 12:33:00 +0100

    Overload find method to find by relative path if a single string argument is provided

--- packages/internals/support/src/collections/file-collection.js
+++ packages/internals/support/src/collections/file-collection.js
@@ -8,6 +8,13 @@ const assert = check.assert;
 
 class FileCollection extends Collection {
 
+  find(...args) {
+    if (args.length === 1 && typeof args[0] === 'string') {
+      return super.find('relative', args[0]);
+    }
+    return super.find(...args);
+  }
+
   filterByPath(...args) {
     let paths = [].concat(...args);
     assert.array.of.string(paths, `FileCollection.filterByPath: path argument must be a string or array of strings [paths-invalid]`);
