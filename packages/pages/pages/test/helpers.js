3bb91c513e0c8a0c18a9df92831ec2db0d869650
Mark Perkins <mark@allmarkedup.com>
2017-09-11 20:54:17 +0100

    Remove pages package to allow merging of prep into v2

--- packages/pages/pages/test/helpers.js
+++
@@ -1,6 +0,0 @@
-const {Fractal} = require('@frctl/fractal');
-const Pages = require('../src/pages');
-
-module.exports.makePages = function (config) {
-  return new Pages(new Fractal(), config);
-};
