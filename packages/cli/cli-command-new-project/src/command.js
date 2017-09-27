f05f02f98a5dad008d84fe4f198a49b653ed0146
Mark Perkins <mark@allmarkedup.com>
2017-09-27 23:04:51 +0100

    Add first pass at new project command

---
+++ packages/cli/cli-command-new-project/src/command.js
@@ -0,0 +1,54 @@
+const {join} = require('path');
+const simpleGit = require('simple-git/promise');
+const tildify = require('tildify');
+const fs = require('fs-extra');
+const shell = require('shelljs');
+
+const defaultStarterUrl = 'https://github.com/frctl/fractal-starter-default';
+
+module.exports = function newProjectCommand() {
+
+  return {
+
+    name: 'fractal-new-project',
+
+    command: 'new <directory-path>',
+
+    description: 'Create a new starter project',
+
+    async handler(argv, app, cli) {
+
+      if (!shell.which('git')) {
+        shell.echo('The Fractal new project command requires Git to be installed on your machine');
+        shell.exit(1);
+      }
+
+      const dirPath = join(cli.cwd, argv.directoryPath);
+      const tildeDir = tildify(dirPath);
+
+      if (fs.existsSync(dirPath)) {
+        throw new Error(`The directory ${argv.directoryPath} already exists in this location. Please choose another name.`);
+      }
+
+      const git = simpleGit(cli.cwd);
+
+      try {
+        await git.clone(defaultStarterUrl, dirPath);
+      } catch(err) {
+        throw new Error(`Failed to clone starter project Git repository [${err.message}]`);
+      }
+
+      shell.cd(argv.directoryPath);
+      const result = shell.exec(`npm install`);
+      if (result.code > 0) {
+        throw new Error(`Failed to install NPM dependencies in the new project`);
+      }
+
+      return `
+        <success>New Fractal starter project created</green>
+
+        Project path: <gray>${tildeDir}</gray>
+      `;
+    }
+  };
+};
