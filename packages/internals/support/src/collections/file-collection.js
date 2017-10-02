ec9642dd821452e756e800c72e32e911f3908540
Mark Perkins <mark@allmarkedup.com>
2017-10-02 12:07:16 +0100

    Remove directories when converting to MemoryFS

--- packages/internals/support/src/collections/file-collection.js
+++ packages/internals/support/src/collections/file-collection.js
@@ -71,7 +71,7 @@ class FileCollection extends EntityCollection {
     // TODO: can we cache this MemoryFs instance creation somehow?
     const memFs = new MemoryFS();
     const errors = [];
-    this.sortBy(file => file.path.length).forEach(file => {
+    this.filter(file => !file.isDirectory()).sortBy(file => file.path.length).forEach(file => {
       try {
         memFs.mkdirpSync(file.dirname);
         memFs.writeFileSync(file.path, file.contents);
