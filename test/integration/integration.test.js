f738f7bfe7e0c1f0aabbb968a6b6fac0c0496d8f
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-10-05 14:07:58 +0100

    Continue addition of Integration tests

--- test/integration/integration.test.js
+++ test/integration/integration.test.js
@@ -1,13 +1,20 @@
 /* eslint import/no-dynamic-require: off,  no-unused-expressions: off */
 const {join} = require('path');
-// const {existsSync} = require('fs');
+const fs = require('fs');
+const pify = require('pify');
+
+const {promisify} = require('../../packages/internals/utils');
 const {expect} = require('../../test/helpers');
 const Fractal = require('../../packages/fractal/fractal');
+
+const pfs = promisify(fs);
 
 const makeConfig = path => ({
   src: join(__dirname, `fixtures/components/${path}`),
   presets: null,
+  // TODO: Intention here, to eventually replace with all engines we provide directly
   engines: [
+    join(__dirname, '../../packages/fractal/fractal-engine-html'),
     join(__dirname, `fixtures/add-ons/engines/funjucks`)
   ]
 });
@@ -16,10 +23,47 @@ function makeFractal(path) {
   return new Fractal(makeConfig(path));
 }
 
-describe.only('Integration', function () {
-  it('parsers and renders fixtures as expected', async function () {
-    const fractal = makeFractal('empty');
-    const components = await fractal.getComponents();
-    expect(components.mapAsync(async component => await fractal.render(component))).to.eventually.be.rejectedWith('[template-not-found]');
+const renderThings = async path => {
+  const fractal = makeFractal(path);
+  const components = await fractal.getComponents();
+  const errors = [];
+
+  const results = await components.mapToArrayAsync(async component => {
+    const getRender = async component => {
+      return await fractal.render(component).catch(err => {
+        errors.push(err);
+        return '';
+      });
+    };
+    return await getRender(component);
+  }).catch(e => {
+    errors.push(e);
+    return [];
+  });
+
+  if (errors.length > 0) {
+    throw new Error(`Could not render Components [component-render-error]\n  ${errors.join('\n')}`);
+  }
+  return results;
+};
+
+const loadExpected = async (path, cb) => {
+  const pathToLoad = join(__dirname, `fixtures/expected/${path}/@button/view.txt`);
+  if (pfs.existsSync(pathToLoad)) {
+    return (await pfs.readFile(pathToLoad)).toString() || '';
+  }
+};
+
+describe('Integration', function () {
+  // Currently just describing current behaviour, but we might want to think about this
+  it('parses and renders fixtures as expected for empty directory', function () {
+    return expect(renderThings('empty-directory')).to.eventually.eql([]);
+  });
+  it('parses and renders fixtures as expected for empty components', function () {
+    return expect(renderThings('empty-component')).to.eventually.be.rejectedWith('[component-render-error]');
+  });
+  it('parses and renders fixtures as expected for simple components', async function () {
+    const expected = await loadExpected('simple');
+    return expect(renderThings('simple')).to.eventually.eql([expected]);
   });
 });
