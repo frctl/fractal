585f4f0124517c599d89b7eb6340550139483cf6
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-10-10 12:16:22 +0100

    Expand simple tests to test both specific and default cases

--- test/integration/integration.test.js
+++ test/integration/integration.test.js
@@ -1,15 +1,9 @@
 /* eslint import/no-dynamic-require: off,  no-unused-expressions: off */
 const {join} = require('path');
-const fs = require('fs');
 const dir = require('path-reader');
-const pify = require('pify');
 
-const {promisify} = require('../../packages/internals/utils');
 const {expect} = require('../../test/helpers');
 const Fractal = require('../../packages/fractal/fractal');
-
-const pfs = promisify(fs);
-const pdir = promisify(dir);
 
 const makeConfig = path => ({
   src: join(__dirname, `fixtures/components/${path}`),
@@ -25,26 +19,41 @@ function makeFractal(path) {
   return new Fractal(makeConfig(path));
 }
 
-const renderComponents = async path => {
+const renderDefaultVariant = async(fractal, errors, path, component) => {
+  const getRender = async component => {
+    const results = [];
+    const result = await fractal.render(component, {}).catch(err => {
+      errors.push(err);
+      return '';
+    });
+    results.push(result);
+    return results;
+  };
+  return await getRender(component);
+};
+
+const renderAllVariants = async(fractal, errors, path, component) => {
+  const getRender = async component => {
+    const results = [];
+    for (const variant of component.getVariants()) {
+      for (const ext of component.getViews().mapToArray(v => v.extname)) {
+        const result = await fractal.render(variant, {}, {ext}).catch(err => {
+          errors.push(err);
+          return '';
+        });
+        results.push(result);
+      }
+    }
+    return results;
+  };
+  return await getRender(component);
+};
+
+const renderAndCatchErrors = async(path, method) => {
   const fractal = makeFractal(path);
   const components = await fractal.getComponents();
   const errors = [];
-  let results = await components.mapToArrayAsync(async component => {
-    const getRender = async component => {
-      const results = [];
-      for (const variant of component.getVariants()) {
-        for (const ext of component.getViews().mapToArray(v => v.extname)) {
-          const result = await fractal.render(variant, {}, {ext}).catch(err => {
-            errors.push(err);
-            return '';
-          });
-          results.push(result);
-        }
-      }
-      return results;
-    };
-    return await getRender(component);
-  }).catch(e => {
+  let results = await components.mapToArrayAsync(method.bind(null, fractal, errors, path)).catch(e => {
     errors.push(e);
     return [];
   });
@@ -55,14 +64,22 @@ const renderComponents = async path => {
   if (errors.length > 0) {
     throw new Error(`Could not render Components [component-render-error]\n  ${errors.join('\n')}`);
   }
-  return results;
+  return results.sort();
 };
 
-const renderExpected = async path => {
+const renderComponentsWithAllVariants = async path => {
+  return await renderAndCatchErrors(path, renderAllVariants);
+};
+
+const renderComponentsWithDefaultVariant = async path => {
+  return await renderAndCatchErrors(path, renderDefaultVariant);
+};
+
+const renderExpected = async(path, match = /.txt$/) => {
   const dirToLoad = join(__dirname, `fixtures/expected/${path}/`);
   return await new Promise((yep, nope) => {
     const results = [];
-    dir.readFiles(dirToLoad,
+    dir.readFiles(dirToLoad, {match: match},
       function (err, content, next) {
         if (err) {
           return nope(err);
@@ -74,25 +91,34 @@ const renderExpected = async path => {
         if (err) {
           return nope(err);
         }
-        return yep(results);
+        return yep(results.sort());
       }
     );
   });
 };
 
 describe('Integration', function () {
-  // Currently just describing current behaviour, but we might want to think about this
-  it('parses and renders fixtures as expected for empty directory', function () {
-    return expect(renderComponents('empty-directory')).to.eventually.eql([]);
+  it('parses and renders all variants as expected for empty directory', function () {
+    return expect(renderComponentsWithAllVariants('empty-directory')).to.eventually.eql([]);
   });
-  it('parses and renders fixtures as expected for empty components', function () {
-    return expect(renderComponents('empty-component')).to.eventually.eql([]);
+  it('parses and renders default variants as expected for empty directory', function () {
+    return expect(renderComponentsWithDefaultVariant('empty-directory')).to.eventually.eql([]);
   });
-  it('parses and renders fixtures as expected for simple components', async function () {
+  it('parses and renders all variants as expected for empty components', function () {
+    return expect(renderComponentsWithAllVariants('empty-component')).to.eventually.eql([]);
+  });
+  // Should this error, or return empty array?
+  it.skip('parses and renders default variants as expected for empty components', function () {
+    return expect(renderComponentsWithDefaultVariant('empty-component')).to.eventually.eql([]);
+  });
+  it('parses and renders default variants as expected for simple components', async function () {
+    const expected = await renderExpected('simple', /.html.txt$/);
+    const comps = await renderComponentsWithDefaultVariant('simple');
+    expect(comps).to.eql(expected);
+  });
+  it('parses and renders all variants as expected for simple components', async function () {
     const expected = await renderExpected('simple');
-    expected.sort();
-    const comps = await renderComponents('simple');
-    comps.sort();
-    return expect(comps).to.eql(expected);
+    const comps = await renderComponentsWithAllVariants('simple');
+    expect(comps).to.eql(expected);
   });
 });
