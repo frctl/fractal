083cb35da399ad9d4d07bb009e1626a497de3ea1
Mark Perkins <mark@allmarkedup.com>
2017-10-11 14:17:29 +0100

    Fix dynamic property handling

--- packages/fractal/fractal-plugin-preprocess-templates/src/eval-attrs.js
+++ packages/fractal/fractal-plugin-preprocess-templates/src/eval-attrs.js
@@ -1,4 +1,8 @@
+const {toArray} = require('@frctl/utils');
 const visit = require('unist-util-visit');
+const getPropInfo = require('property-information');
+const commaSep = require('comma-separated-tokens');
+const spaceSep = require('space-separated-tokens');
 const safeEval = require('./eval');
 
 module.exports = function (tree, env) {
@@ -6,26 +10,38 @@ module.exports = function (tree, env) {
     if (node.properties) {
       for (const key of Object.keys(node.properties)) {
         if (key[0] === ':') {
-          const propRealName = key.slice(1);
-          let result = safeEval(node.properties[key], env);
-
-          if (Array.isArray(result)) {
-            result = result.join(' ');
-          }
-          if (typeof result !== 'string') {
-            throw new Error(`attribute '${key}' must resolve to a string`);
-          }
+          const propName = key.slice(1);
+          const propInfo = getPropInfo(propName);
+          const name = propInfo ? propInfo.propertyName : propName;
+          const result = safeEval(node.properties[key], env);
 
           delete node.properties[key];
 
-          if (node.properties[propRealName] && node.properties[propRealName].trim() !== '') {
-            node.properties[propRealName] += ` ${result}`;
-          } else {
-            node.properties[propRealName] = result;
-          }
+          node.properties[name] = resolveValue(node.properties[name], result, propInfo);
         }
       }
     }
   });
   return tree;
 };
+
+function resolveValue(existing, additional, propInfo) {
+  if (!propInfo) {
+    return toArray(existing).concat(toArray(additional)).join(' ');
+  }
+  if (propInfo.boolean || propInfo.overloadedBoolean) {
+    return Boolean(additional);
+  }
+  if (propInfo.spaceSeparated || propInfo.commaSeparated) {
+    // result should be an array
+    if (typeof additional === 'string' && propInfo.spaceSeparated) {
+      additional = spaceSep.parse(additional);
+    } else if (typeof additional === 'string' && propInfo.commaSeparated) {
+      additional = commaSeparated.parse(additional);
+    }
+    return toArray(existing).concat(toArray(additional));
+  }
+  if (propInfo.numeric || propInfo.positiveNumeric) {
+    return parseInt(additional, 10);
+  }
+}
--- packages/fractal/fractal-plugin-preprocess-templates/src/plugin.js
+++ packages/fractal/fractal-plugin-preprocess-templates/src/plugin.js
@@ -1,5 +1,4 @@
 const evalAttrs = require('./eval-attrs');
-// const includeComponents = require('./include-components');
 
 module.exports = function (opts = {}) {
   return {
@@ -12,11 +11,8 @@ module.exports = function (opts = {}) {
       components.forEach(component => {
         component.getVariants().forEach(variant => {
           variant.getTemplates().forEach(template => {
-            const env = {template, component, variant, components};
-            const tree = template.contents;
-            evalAttrs(tree, env);
-            // includeComponents(tree, env);
-            template.contents = tree;
+            const env = {template, component, variant, components, this: variant};
+            evalAttrs(template.contents, env);
           });
         });
       });
