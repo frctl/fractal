d753f0f6a348f9134d54d83cc012baa7c51b0baf
Mark Perkins <mark@allmarkedup.com>
2017-10-19 15:47:57 +0200

    Add very basic if-statement to template preprocessing

---
+++ packages/fractal/fractal-plugin-preprocess-templates/src/logic.js
@@ -0,0 +1,16 @@
+const {toArray} = require('@frctl/utils');
+const visit = require('unist-util-visit');
+const safeEval = require('./eval');
+
+module.exports = function (tree, env) {
+  visit(tree, 'element', function (node, index, parentNode) {
+    if (node.properties['@if']) {
+      const result = safeEval(node.properties['@if'], env);
+      if (!result) {
+        // false, remove the node
+        parentNode.children.splice(index, 1);
+      }
+    }
+  });
+  return tree;
+};
