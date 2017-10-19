5fc127c046ff8ce7dd3ccdbb41e77077be57ae7e
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-10-19 12:17:58 +0100

    Add Async methods to FileSystemStack

--- packages/internals/support/src/fs/fs-stack.js
+++ packages/internals/support/src/fs/fs-stack.js
@@ -1,7 +1,24 @@
 const {toArray} = require('@frctl/utils');
+const {tryEach} = require('async');
 
 const _stack = new WeakMap();
-const fsMethods = [
+
+const noErrMethods = [
+  "exists" // exists (no err arg)
+];
+const fsAsyncMethods = [
+  "mkdir", // err
+  "readFile", // err, data,
+  "writeFile", // err
+  "stat", // err, stats
+  "readdir", // err, files
+  "mkdirp", // err, files
+  "rmdir", // err
+  "unlink", // err
+  "readlink", // err, linkString
+];
+
+const fsSyncMethods = [
   "statSync",
   "readdirSync",
   "mkdirpSync",
@@ -9,30 +26,25 @@ const fsMethods = [
   "rmdirSync",
   "unlinkSync",
   "readlinkSync",
-
-  "stat",
-  "readdir",
-  "mkdirp",
-  "mkdir",
-  "rmdir",
-  "unlink",
-  "readlink",
-
-  "meta",
   "existsSync",
   "readFileSync",
-  "_remove",
   "writeFileSync",
   "join",
   "normalize",
-  "exists",
-  "readFile",
-  "writeFile",
+  "_remove",
+  "meta",
 ];
 
 class FileSystemStack {
 
   constructor(fileSystemStack = []) {
+    fileSystemStack = toArray(fileSystemStack).map(fileSystem => {
+      fileSystem._existsAsync = (path, cb) => {
+        fileSystem.exists(path, exists => cb(null, exists));
+      };
+      return fileSystem;
+    });
+
     _stack.set(this, toArray(fileSystemStack));
   }
 
@@ -42,8 +54,8 @@ class FileSystemStack {
 
 }
 
-fsMethods.forEach(method => {
-  FileSystemStack.prototype[method] = function (...args) {
+fsSyncMethods.forEach(method => {
+  FileSystemStack.prototype[method] = function(...args) {
     const stack = _stack.get(this);
     for (var i = 0; i < stack.length; i++) {
       try {
@@ -58,4 +70,30 @@ fsMethods.forEach(method => {
   };
 });
 
+fsAsyncMethods.forEach(method => {
+  FileSystemStack.prototype[method] = function(...args) {
+    const stack = _stack.get(this);
+    const tasks = stack.map(fs => fs[method].bind(fs));
+
+    const callback = args.pop();
+
+    tryEach(tasks, (err, results) => {
+      return callback(err, results);
+    });
+  };
+});
+
+FileSystemStack.prototype.exists = function(...args) {
+  const stack = _stack.get(this);
+  const tasks = stack.map(fs => fs._existsAsync.bind(fs));
+  const callback = args.pop();
+
+  tryEach(tasks, (err, results) => {
+    return callback(results);
+  });
+}
+
 module.exports = FileSystemStack;
+module.exports.fsSyncMethods = fsSyncMethods;
+module.exports.fsAsyncMethods = fsAsyncMethods;
+module.exports.noErrMethods = noErrMethods;
---
+++ packages/internals/support/src/fs/fs-stack.test.js
@@ -0,0 +1,85 @@
+/* eslint import/no-unresolved: off */
+
+const {expect} = require('../../../../../test/helpers');
+const FileSystemStack = require('./fs-stack');
+
+const fsSyncMethods = [].concat(FileSystemStack.fsSyncMethods);
+const fsAsyncMethods = [].concat(FileSystemStack.fsAsyncMethods, FileSystemStack.noErrMethods);
+
+const makeFs = throwErr => {
+  const fs = {
+    called: null
+  };
+  for (const method of fsSyncMethods) {
+    fs[method] = function() {
+      this.called = method;
+      if (throwErr) {
+        throw new Error('oops');
+      }
+    };
+  }
+  for (const method of fsAsyncMethods) {
+    fs[method] = function(callback) {
+      this.called = method;
+      if (throwErr) {
+        return callback(new Error('oops'));
+      }
+      callback(null, true);
+    };
+  }
+  return fs;
+};
+
+
+describe('FileSystemStack', function() {
+  for (const fsMethod of fsSyncMethods) {
+    describe(`.${fsMethod}()`, function() {
+      it('calls the sync methods on each obj in the stack until one does not throw an error', function() {
+        const fs1 = makeFs(true);
+        const fs2 = makeFs(true);
+        const fs3 = makeFs(false);
+        const fs4 = makeFs(false);
+        const stack = new FileSystemStack([fs1, fs2, fs3, fs4]);
+        stack[fsMethod]();
+        expect(fs1.called).to.equal(fsMethod);
+        expect(fs2.called).to.equal(fsMethod);
+        expect(fs3.called).to.equal(fsMethod);
+        expect(fs4.called).to.equal(null);
+      });
+    });
+  }
+  for (const fsMethod of fsAsyncMethods) {
+    describe(`.${fsMethod}()`, function() {
+      it('calls the async methods on each obj in the stack until one does not throw an error', function(done) {
+        const fs1 = makeFs(true);
+        const fs2 = makeFs(true);
+        const fs3 = makeFs(false);
+        const fs4 = makeFs(false);
+        const stack = new FileSystemStack([fs1, fs2, fs3, fs4]);
+        stack[fsMethod]((err, result) => {
+          expect(fs1.called).to.equal(fsMethod);
+          expect(fs2.called).to.equal(fsMethod);
+          expect(fs3.called).to.equal(fsMethod);
+          expect(fs4.called).to.equal(null);
+          done();
+        });
+      });
+    });
+  }
+  describe(`.exists()`, function(done) {
+    it('calls the async methods on each obj in the stack until one does not throw an error', function(done) {
+      const fs1 = makeFs(true);
+      const fs2 = makeFs(true);
+      const fs3 = makeFs(false);
+      const fs4 = makeFs(false);
+      const stack = new FileSystemStack([fs1, fs2, fs3, fs4]);
+      stack.exists(result => {
+        expect(fs1.called).to.equal('exists');
+        expect(fs2.called).to.equal('exists');
+        expect(fs3.called).to.equal('exists');
+        expect(fs4.called).to.equal(null);
+        done();
+      });
+    });
+  });
+});
