22a6b7e798f3998bb6be2c95b729a4d47b81b89c
Mark Perkins <mark@allmarkedup.com>
2017-10-23 16:41:51 +0100

    Track includes in variants

--- packages/fractal/fractal-plugin-preprocess-templates/src/include.js
+++ packages/fractal/fractal-plugin-preprocess-templates/src/include.js
@@ -5,14 +5,15 @@ const removePosition = require('unist-util-remove-position');
 
 module.exports = function (tree, context, env) {
   const parent = env.component;
+  const variant = env.variant;
 
   visit(tree, 'element', function (node, index, parentNode) {
     if (is(node, 'include')) {
       if (!node.properties.component) {
         throw new Error(`You must provide a 'component' attribute for the 'include' tag ${parent.id}`);
       }
-      const [componentId, variantId] = node.properties.component.split(':');
-      const subComponent = env.components.find(componentId);
+      const [subComponentId, variantId] = node.properties.component.split(':');
+      const subComponent = env.components.find(subComponentId);
       if (subComponent.id === parent.id) {
         throw new Error(`Recursive component include detected! Ignoring component.`);
       }
@@ -23,6 +24,22 @@ module.exports = function (tree, context, env) {
 
       if (!template) {
         throw new Error(`Could not find '${templateExt}' template for component ${subComponent.ifd}`);
+      }
+
+      variant.includes = variant.get('includes', []);
+
+      const include = {
+        component: subComponent.id,
+        variant: subComponentVariant.id,
+        template: templateExt,
+        count: 1,
+        ref: `${subComponent.id}.${subComponentVariant.id}${templateExt}`
+      };
+
+      if (variant.includes.find(i => i.ref === include.ref)) {
+        included.count++;
+      } else {
+        variant.includes.push(include);
       }
 
       const componentNodes = removePosition(template.clone().contents).children;
