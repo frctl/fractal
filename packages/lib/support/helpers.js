3d4876a854c9b50baef102e14b233e3c6114f289
Mark Perkins <mark@allmarkedup.com>
2017-11-13 15:56:06 +0000

    Add lookup helper

--- packages/lib/support/helpers.js
+++ packages/lib/support/helpers.js
@@ -1,16 +1,19 @@
 const {extname} = require('path');
+const ComponentCollection = require('./src/collections/component-collection');
 
 module.exports = {
 
-  getPartials(components = [], matcher) {
+  getPartials(components, matcher) {
+    components = components || new ComponentCollection();
     if (!matcher) {
       throw new Error('You must specify a template extension matcher [matcher-missing]');
     }
     if (typeof matcher !== 'function') {
+      const extensions = [].concat(matcher);
       matcher = filename => {
         const ext = extname(filename);
-        return [].concat(matcher).includes(ext);
-      }
+        return extensions.includes(ext);
+      };
     }
     const partials = {};
     for (const component of components) {
@@ -19,13 +22,24 @@ module.exports = {
         if (tpl) {
           const str = tpl.toString();
           partials[`${component.id}:${variant.id}`] = str;
+          partials[`${component.id}:${variant.id}${tpl.extname}`] = str;
           if (component.isDefaultVariant(variant)) {
             partials[component.id] = str;
+            partials[`${component.id}${tpl.extname}`] = str;
           }
         }
       }
     }
     return partials;
+  },
+
+  lookup(ref, components) {
+    components = components || new ComponentCollection();
+    const [componentName, variantName] = ref.split(':');
+    const component = components.find(componentName);
+    if (component) {
+      return variantName ? component.getVariant(variantName) : component.getDefaultVariant();
+    }
   }
 
 };
