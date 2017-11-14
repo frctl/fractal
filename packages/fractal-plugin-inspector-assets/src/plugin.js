13503a442510dcb4e5b75ad7bdbd0a0ed0ed5975
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-11-14 17:55:20 +0000

    WIP

--- packages/fractal-plugin-inspector-assets/src/plugin.js
+++ packages/fractal-plugin-inspector-assets/src/plugin.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const webpack = require('webpack');
 const MemoryFS = require('memory-fs');
 const {promisify} = require('@frctl/utils');
-const {FileCollection, FileSystemStack} = require('@frctl/support');
+const {FileCollection, FileSystemStack, ComponentCollection} = require('@frctl/support');
 const debug = require('debug')('frctl:plugin:inspector-assets');
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -12,11 +12,67 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const styleTest = /\.(sass|scss|css)$/;
 const fileTest = /\.(svg|png|jpg|jpeg|gif|webm|mp4|ogg|woff|woff2)$/;
 
-const getConfig = component => {
+const getPathHead = (modulePath) => {
+  return modulePath.split(path.sep)[0]
+}
+const getPathTail = (modulePath) => {
+  return modulePath.split(path.sep).slice(1).join(path.sep);
+}
+
+const ComponentResolver = function(options) {
+  this.components = options.components;
+};
+
+ComponentResolver.prototype.apply = function(resolver) {
+  const components = this.components;
+
+  resolver.plugin('module', function(shortRequest, callback) {
+
+    const shortPath = shortRequest.request;
+    // console.log(shortRequest);
+
+    // ignore relative or absolute paths
+    if ( /^\W+/.test(shortPath)){
+      return callback();
+    } else {
+      console.log('ADDING PATH', shortPath);
+    }
+
+    const componentId = getPathHead(shortPath);
+    const requestTail = getPathTail(shortPath);
+
+    const component = components.find('id', componentId);
+
+    //ignore paths which don't match a component
+    if (!component) {
+      return callback();
+    }
+
+    console.log(shortPath, componentId, requestTail);
+    //resolve the long path
+    var longPath = component.path;
+
+    // console.log(path.join(longPath, requestTail))
+
+    //create a new request to resolve the full path
+    const longRequest = {
+      path: path.join(longPath, requestTail),
+      query: shortRequest.query,
+      file: true, resolved: true
+    };
+
+    // console.log('longreq', longRequest);
+
+    //resolve the long path
+    return resolver.doResolve(['file'], longRequest, 'expanded component path \'' + shortPath + '\' to \'' + path.join(longPath, requestTail) + '\'', callback);
+  });
+}
+
+const getConfig = (component, components) => {
   const basePath = component.getSrc().path;
   const stem = component.getSrc().stem;
   const extractSASS = new ExtractTextPlugin(`${basePath}/${stem}.css`);
-
+  const componentResolver = new ComponentResolver({components});
   return {
     context: process.cwd(),
     entry: component.getAssets().toArray().map(file => file.path),
@@ -25,6 +81,9 @@ const getConfig = component => {
       path: `${path.join(basePath)}`
     },
     resolve: {
+      plugins: [
+        componentResolver
+      ],
       modules: [path.join(process.cwd(), 'node_modules')]
     },
     module: {
@@ -60,9 +119,9 @@ module.exports = function (opts = {}) {
 
     transform: 'components',
 
-    async handler(components, state, app) {
+    async handler(_components, state, app) {
       // FIXME: looping/compiling need to be optimised here
-      components = await components.mapAsync(async component => {
+      const components = await _components.mapAsync(async component => {
         const outputFileSystem = new MemoryFS();
         component.inspector = component.inspector || {};
         const assets = component.getAssets();
@@ -72,18 +131,21 @@ module.exports = function (opts = {}) {
         }
         assets.toMemoryFS(memoryFs);
 
-        const config = getConfig(component);
+        const config = getConfig(component, _components);
         const compiler = webpack(config);
         const pRun = promisify(compiler.run.bind(compiler));
 
         compiler.inputFileSystem = inputFileSystem;
         compiler.resolvers.normal.fileSystem = compiler.inputFileSystem;
         compiler.resolvers.context.fileSystem = compiler.inputFileSystem;
+        compiler.resolvers.loader.fileSystem = compiler.inputFileSystem;
         compiler.outputFileSystem = outputFileSystem;
 
-        await pRun().catch(err => {
+        const res = await pRun().catch(err => {
           debug(`Error running plugin handler: ${err}`);
         });
+        console.log(res);
+
         component.inspector.assets = await FileCollection.fromMemoryFS(outputFileSystem); // TODO: should this overwrite, or merge in some way?
         debug(`
 component: ${util.inspect(component.inspector.assets.mapToArray(file => ({path: file.path, contents: file.contents.toString()})), {showHidden: false, depth: 4})}
--- packages/fractal-plugin-inspector-assets/src/plugin.test.js
+++ packages/fractal-plugin-inspector-assets/src/plugin.test.js
@@ -18,7 +18,7 @@ var t = padStart('${name}', 8);`)
     }),
     new File({
       path: `${__dirname}/../components/${name}/${name}.view.hbs`,
-      contents: new Buffer(`<button class=${name}>${name}</button>`, 'utf-8')
+      contents: new Buffer(`<button class="${name}">${name}</button>`, 'utf-8')
     }),
     new File({
       path: `${__dirname}/../components/${name}/${name}.scss`,
@@ -115,6 +115,116 @@ color: $red;
 const makeCollection = () => ComponentCollection.from([makeComponent('one'), makeComponent('two')]);
 const makeAssetlessCollection = () => ComponentCollection.from([makeEmptyComponent('three'), makeEmptyComponent('four')]);
 const makeSingleAssetCollection = (name, type) => ComponentCollection.from([makeSingleAssetComponent(name, type)]);
+const makeDependentComponents = () => ComponentCollection.from([
+  Component.from({
+    src: new File({
+      path: `${__dirname}/../components/button`,
+      stat: {
+        isDirectory: () => true
+      }
+    }),
+    files: FileCollection.from([
+      new File({
+        path: `${__dirname}/../components/button/button.js`,
+        contents: new Buffer(`
+  console.log('button');`)
+      }),
+      new File({
+        path: `${__dirname}/../components/button/button.view.hbs`,
+        contents: new Buffer(`<button class="button">button</button>`, 'utf-8')
+      }),
+      new File({
+        path: `${__dirname}/../components/button/button.scss`,
+        contents: new Buffer(`
+  $red: #f00;
+  .button {
+    color: $red;
+  }`, 'utf-8')
+}),
+      new File({
+        path: `${__dirname}/../components/button/preview.js`,
+        contents: new Buffer(`
+require('./button.scss');
+module.exports = function(scenario) {
+  require('./button.js');
+}`)
+      })
+    ]),
+    config: {
+      id: `button`,
+      assets: {
+        scripts: '**/*.js',
+        styles: '**/*.{scss,css}',
+        images: '**/*.{png,jpg}'
+      }
+    }
+  }),
+  Component.from({
+    src: new File({
+      path: `${__dirname}/../components/card`,
+      stat: {
+        isDirectory: () => true
+      }
+    }),
+    files: FileCollection.from([
+      new File({
+        path: `${__dirname}/../components/card/card.js`,
+        contents: new Buffer(`
+module.exports = function(){
+  require('button/button.js');
+  console.log('card')
+}`)
+      }),
+      new File({
+        path: `${__dirname}/../components/card/card.view.hbs`,
+        contents: new Buffer(`
+          <div class="card">
+            <h3 class="card__title">{{ title }}</h3>
+            <p class="card__content">{{ text }}</p>
+          </div>`, 'utf-8')
+      }),
+      new File({
+        path: `${__dirname}/../components/card/card.scss`,
+        contents: new Buffer(`
+@import '~button/button.scss';
+$border-color: #ccc;
+.card {
+  border: 1px solid $border-color;
+  box-shadow: 2px 2px 2px rgba(0,0,0,0.2);
+  max-width: 500px;
+  border-radius: 4px;
+  padding: 1em;
+  font-family: sans-serif;
+}
+.card__title {
+  margin: 0.4em 0 0.5em 0;
+}
+.card__content {
+  line-height: 1.4;
+}`, 'utf-8')
+      }),
+    new File({
+      path: `${__dirname}/../components/card/preview.js`,
+      contents: new Buffer(`
+require('./card.scss');
+const card = require('./card.js');
+
+module.exports = function(scenario){
+  card({
+    text: scenario.context.text
+  });
+}`)})
+]),
+    config: {
+      id: `card`,
+      assets: {
+        scripts: '**/*.js',
+        styles: '**/*.{scss,css}',
+        images: '**/*.{png,jpg}'
+      }
+    }
+  })
+])
 
 tests.addPluginTest({
   description: 'sets an inspector.asset property on each component',
@@ -172,6 +282,19 @@ tests.addPluginTest({
   }
 });
 
+tests.addPluginTest({
+  description: 'sets an inspector.asset property on each component when the components have dependencies on each other',
+  input: makeDependentComponents(),
+  timeout: 3000,
+  test: function (collection) {
+    for (const component of collection) {
+      component.inspector.assets.toArray().map(file => console.log('\n', file.path, file.contents.toString()));
+      expect(component.inspector.assets).to.be.a('FileCollection');
+      expect(component.inspector.assets.toArray().map(file => file.path).length).to.equal(2);
+    }
+  }
+});
+
 // tests.addPluginTest({
 //  description: 'it works as a dependency'
 // })
@@ -180,4 +303,4 @@ tests.addPluginTest({
 //  description: 'it works for media and font assets'
 // })
 
-tests.run();
+tests.runOnly();
