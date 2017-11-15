00ac595bb53423f370fe21de9660d4e142132206
Mark Perkins <mark@allmarkedup.com>
2017-11-15 15:01:34 +0000

    Bind template helpers to object with variant and component access

--- packages/fractal-plugin-preprocess-templates/src/plugin.js
+++ packages/fractal-plugin-preprocess-templates/src/plugin.js
@@ -9,10 +9,18 @@ module.exports = function (opts = {}) {
 
     async handler(components, state, app) {
       const globals = app.get('components.templates.globals', {});
+      const helpers = Object.assign({}, app.get('components.templates.helpers', {}));
 
       components.forEach(component => {
         component.getVariants().forEach(variant => {
+          // context includes variant props, globals and helper functions.
+          // helper functions are bound to the variant object itself.
           const context = Object.assign({}, variant.props, globals);
+          Object.keys(helpers).forEach(name => {
+            if (typeof helpers[name] === 'function') {
+              context[name] = helpers[name].bind({component, variant});
+            }
+          });
 
           variant.getTemplates().forEach(template => {
             const env = {template, component, variant, components};
--- packages/fractal/src/config/defaults.js
+++ packages/fractal/src/config/defaults.js
@@ -22,7 +22,8 @@ module.exports = {
       }
     },
     templates: {
-      globals: {}
+      globals: {},
+      helpers: {},
     }
   },
 
