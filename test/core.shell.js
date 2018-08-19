06fe7ebedefdbe52d4772b31e74ce217f8aa62e6
Matt Chapman <chapabu101@gmail.com>
2018-08-19 21:44:58 +0100

    Add first pass tests and dependencies.
    * Added test coverage for all shell methods used.
    * Added mock-fs as development dependency.
    * Tests commented (temporarily) to allow commit.

---
+++ test/core.shell.js
@@ -0,0 +1,54 @@
+const chai = require('chai');
+const expect = chai.expect;
+const mock = require('mock-fs');
+const path = require('path');
+const fs = require('fs');
+
+const shell = require('../src/core/shell');
+
+describe('Shell', function() {
+
+    let originalPwd;
+
+    beforeEach(function() {
+        originalPwd = process.cwd();
+        mock({
+            'shelltest': {
+                'nested-directory': {},
+                'empty-file.txt': ''
+            }
+        });
+    });
+
+    afterEach(function() {
+        // Ensure the current working directory is the same as when we started the test suite.
+        process.cwd(originalPwd);
+        // Reset the filesystem.
+        mock.restore();
+    })
+
+    it('can change directory', function() {
+        const cwd = process.cwd();
+        shell.cd('shelltest');
+        expect(process.cwd()).to.equal(`${cwd}${path.sep}shelltest`);
+    })
+
+    // it('can create a file', function(done) {
+    //     shell.touch('test-file.md');
+    //     fs.stat(path.join(process.cwd(), 'test-file.md'), function(error, stats) {
+    //         if (error) done(error)
+    //         expect(stats.isFile()).to.be.true;
+    //         done();
+    //     })
+    // });
+
+    // it('can execute a child process', function(done) {
+    //     // This should be OK cross-platform.
+    //     // If not - any suggestions?
+    //     const cmd = shell.exec('node -v');
+    //     cmd.stdout.on('data', function(data) {
+    //         expect(data.toString()).to.equal(process.version);
+    //         done();
+    //     })
+    // });
+});
