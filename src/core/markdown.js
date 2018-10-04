31158e69705fea2f9af84e5bfd89b766dbd361f9
Mark Perkins <mark@allmarkedup.com>
2018-10-04 09:46:06 +0300

    Prevent markdown renderer from directly mutating config object

--- src/core/markdown.js
+++ src/core/markdown.js
@@ -1,19 +1,22 @@
-'use strict';
+"use strict";
 
-const marked = require('marked');
-const _ = require('lodash');
-const highlighter = require('./highlighter');
+const marked = require("marked");
+const _ = require("lodash");
+const highlighter = require("./highlighter");
 const renderer = new marked.Renderer();
 
-renderer.code = function (code, lang, escaped) {
-    const output = highlighter(code, lang);
-    if (output != null) {
-        code = output;
-    }
-    if (!lang) {
-        return `<code><pre>${code}</pre></code>`;
-    }
-    return `<code class="${this.options.langPrefix}${escape(lang, true)}"><pre>${code}</pre></code>`;
+renderer.code = function(code, lang, escaped) {
+  const output = highlighter(code, lang);
+  if (output != null) {
+    code = output;
+  }
+  if (!lang) {
+    return `<code><pre>${code}</pre></code>`;
+  }
+  return `<code class="${this.options.langPrefix}${escape(
+    lang,
+    true
+  )}"><pre>${code}</pre></code>`;
 };
 
 /*
@@ -21,23 +24,25 @@ renderer.code = function (code, lang, escaped) {
  */
 
 module.exports = function markdown(content, mdConfig) {
-    mdConfig = (mdConfig && _.isObject(mdConfig)) ? mdConfig : {};
-    mdConfig.renderer = renderer;
+  mdConfig = _.cloneDeep(mdConfig && _.isObject(mdConfig) ? mdConfig : {});
+  mdConfig.renderer = renderer;
 
-    return marked(_.toString(content), mdConfig);
+  return marked(_.toString(content), mdConfig);
 };
 
-module.exports.toc = function (content, maxDepth, mdConfig) {
-    maxDepth = maxDepth || 6;
-    mdConfig = (mdConfig && _.isObject(mdConfig)) ? mdConfig : {};
-    mdConfig.renderer = renderer;
+module.exports.toc = function(content, maxDepth, mdConfig) {
+  maxDepth = maxDepth || 6;
+  mdConfig = mdConfig && _.isObject(mdConfig) ? mdConfig : {};
+  mdConfig.renderer = renderer;
 
-    const tokens = marked.lexer(_.toString(content));
+  const tokens = marked.lexer(_.toString(content));
 
-    return tokens.filter(token => {
-        return token.type === 'heading' && token.depth <= maxDepth;
-    }).map(token => {
-        token.id = token.text.toLowerCase().replace(/[^\w]+/g, '-');
-        return token;
+  return tokens
+    .filter(token => {
+      return token.type === "heading" && token.depth <= maxDepth;
+    })
+    .map(token => {
+      token.id = token.text.toLowerCase().replace(/[^\w]+/g, "-");
+      return token;
     });
 };
