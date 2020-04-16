461467631b04b9eacc0f390372369de5d23418c7
Mihkel Eidast <mihkel@play.ee>
2020-04-16 18:02:25 +0300

    resolve date in config to JSON string

--- src/core/resolver.js
+++ src/core/resolver.js
@@ -66,6 +66,10 @@ const resolver = module.exports = {
                 return null;
             }
 
+            if (_.isDate(item)) {
+                return item.toJSON();
+            }
+
             if (_.isArray(item) || _.isObject(item)) {
                 return resolve(item);
             }
