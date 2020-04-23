dfdaf21d4ed96d70a458905a8e36395d31e1760f
Mihkel Eidast <mihkel@play.ee>
2020-04-23 19:39:55 +0300

    add hljs class to code elements

--- src/core/markdown.js
+++ src/core/markdown.js
@@ -11,9 +11,9 @@ renderer.code = function(code, lang, escaped) {
     code = output;
   }
   if (!lang) {
-    return `<pre><code>${code}</code></pre>`;
+    return `<pre><code class="hljs">${code}</code></pre>`;
   }
-  return `<pre><code class="${this.options.langPrefix}${escape(
+  return `<pre><code class="hljs ${this.options.langPrefix}${escape(
     lang,
     true
   )}">${code}</code></pre>`;
