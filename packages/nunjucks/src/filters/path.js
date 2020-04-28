78e23ca013aa8214d515cb4b0baa5e600d149583
Ken Hawkins <khawkins98@gmail.com>
2020-04-28 19:28:45 +0200

    fix: fetch request context for README.md (#13)
    When using the njk extension for README.md notes, the request isn't available.
    
    That means doing something like: `{{ '/assets/vf-figure/assets/figure-example.png' | path}}` fails as the `path` filter doesn't know what URL the request is coming from.

--- packages/nunjucks/src/filters/path.js
+++ packages/nunjucks/src/filters/path.js
@@ -8,7 +8,7 @@ module.exports = function(fractal) {
     return function(path) {
 
         let env = this.lookup('_env');
-        let request = env.request || this.lookup('_request');
+        let request = env.request || this.lookup('_request') || this.ctx.request;
 
         return (! env || env.server) ? path : utils.relUrlPath(path, _.get(request, 'path', '/'), fractal.web.get('builder.urls'));
     }
