a8daf61ee7cc8b25b4301281d36b54dbb0036df7
Benoît Burgener <benoit.burgener@gmail.com>
2020-05-02 10:41:15 +0200

    feat(Navigation): improve search behavior & add collapse all (#107)

---
+++ packages/mandelbrot/assets/js/components/navigation.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const $ = global.jQuery;
+const Tree = require('./tree');
+const Search = require('./search');
+
+class Navigation {
+
+    constructor(el) {
+        this._el = $(el);
+        this.navTrees = $.map(this._el.find('[data-behaviour="tree"]'), t => new Tree(t));
+        $.map(this._el.find('[data-behaviour="search"]'), s => new Search(s, this.navTrees));
+    }
+
+}
+
+module.exports = Navigation;
--- packages/mandelbrot/assets/js/components/search.js
+++ packages/mandelbrot/assets/js/components/search.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const $ = global.jQuery;
+const Mark = require('mark.js');
 
 class Search {
 
@@ -8,15 +9,48 @@ class Search {
         this._el = $(el);
         this._trees = trees;
         this._input = this._el.find('[data-role="input"]');
+        this._clearButton = this._el.find('[data-behaviour="clear-search"]');
+        this._marker = new Mark($.map(this._trees, t => t.getElement()[0]));
 
         this._el.on('submit', (event) => {
             event.preventDefault();
         })
 
-        this._input.on('input', (event) => {
-            const key = event.currentTarget.value.toUpperCase();
+        this._input.on('input', this.handleInput.bind(this));
 
-            trees.forEach((tree) => this.search(tree._el.children('ul'), key, tree._collections));
+        if (this._clearButton) {
+            this._clearButton.on('click', () => {
+                this._input.val('').trigger('input');
+            });
+        }
+    }
+
+    handleInput(event) {
+        const key = event.currentTarget.value.toUpperCase();
+
+        if (this._clearButton) {
+            if (key) {
+                this._clearButton.removeAttr('hidden');
+            } else {
+                this._clearButton.attr('hidden', true);
+            }
+        }
+
+        this._marker.unmark();
+        this._marker.mark(key);
+
+        this._trees.forEach((tree) => {
+            this.search(tree._el.children('ul'), key, tree._collections)
+
+            // If no item match in the group, hide it completely
+            const $treeEl = $(tree._el);
+            const $treeGroup = $treeEl.parent('.Navigation-group');
+
+            if ($treeEl.find('> ul > li:not([hidden])').length === 0) {
+                $treeGroup.attr('hidden', true);
+            } else {
+                $treeGroup.removeAttr('hidden');
+            }
         });
     }
 
@@ -35,19 +69,21 @@ class Search {
             const tagAttributesMatch = tagAttributes.toUpperCase().indexOf(key) !== -1;
 
             if (collectionLabelMatches || itemLabelMatches || tagAttributesMatch) {
-                if (key.length) {
-                    $li.parents('.Tree-collection').each((_index, parent) => {
-                        const collection = collections.find((c) => c._el[0] === parent);
+                $li.parents('.Tree-collection').each((_index, parent) => {
+                    const collection = collections.find((c) => c._el[0] === parent);
 
-                        collection.open();
-                    });
-                }
-                $li.show();
+                    if (key.length || collection.containsCurrentItem()) {
+                        collection.open(true);
+                    } else {
+                        collection.close(true);
+                    }
+                });
+                $li.removeAttr('hidden');
 
                 // recursive search inside nested collections
                 this.search($li.children('ul'), key, collections);
             } else {
-                $li.hide();
+                $li.attr('hidden', true);
             }
         });
     }
