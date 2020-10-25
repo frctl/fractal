9e31181b1cbe700b4849af0d0375ffab41a7613d
Mihkel Eidast <mihkel@play.ee>
2020-10-25 21:53:34 +0200

    undo labels object split

--- packages/mandelbrot/src/theme.js
+++ packages/mandelbrot/src/theme.js
@@ -6,53 +6,6 @@ const Theme = require('@frctl/web').Theme;
 const packageJSON = require('../package.json');
 
 module.exports = function (options) {
-    const labels = _.defaultsDeep(_.clone((options || {}).labels || {}), {
-        info: 'Information',
-        builtOn: 'Built on',
-        search: {
-            label: 'Search',
-            placeholder: 'Search…',
-            clear: 'Clear search',
-        },
-        tree: {
-            collapse: 'Collapse tree',
-        },
-        components: {
-            handle: 'Handle',
-            tags: 'Tags',
-            variants: 'Variants',
-            context: {
-                empty: 'No context defined.',
-            },
-            notes: {
-                empty: 'No notes defined.',
-            },
-            preview: {
-                label: 'Preview',
-                withLayout: 'With layout',
-                componentOnly: 'Component only',
-            },
-            path: 'Filesystem Path',
-            references: 'References',
-            referenced: 'Referenced by',
-            resources: {
-                file: 'File',
-                content: 'Content',
-                previewUnavailable: 'Previews are currently not available for this file type.',
-                url: 'URL',
-                path: 'Filesystem Path',
-                size: 'Size',
-            },
-        },
-        panels: {
-            html: 'HTML',
-            view: 'View',
-            context: 'Context',
-            resources: 'Resources',
-            info: 'Info',
-            notes: 'Notes',
-        },
-    });
     const config = _.defaultsDeep(_.clone(options || {}), {
         skin: 'default',
         rtl: false,
@@ -66,7 +19,53 @@ module.exports = function (options) {
         },
         version: packageJSON.version,
         favicon: null,
-        labels,
+        labels: {
+            info: 'Information',
+            builtOn: 'Built on',
+            search: {
+                label: 'Search',
+                placeholder: 'Search…',
+                clear: 'Clear search',
+            },
+            tree: {
+                collapse: 'Collapse tree',
+            },
+            components: {
+                handle: 'Handle',
+                tags: 'Tags',
+                variants: 'Variants',
+                context: {
+                    empty: 'No context defined.',
+                },
+                notes: {
+                    empty: 'No notes defined.',
+                },
+                preview: {
+                    label: 'Preview',
+                    withLayout: 'With layout',
+                    componentOnly: 'Component only',
+                },
+                path: 'Filesystem Path',
+                references: 'References',
+                referenced: 'Referenced by',
+                resources: {
+                    file: 'File',
+                    content: 'Content',
+                    previewUnavailable: 'Previews are currently not available for this file type.',
+                    url: 'URL',
+                    path: 'Filesystem Path',
+                    size: 'Size',
+                },
+            },
+            panels: {
+                html: 'HTML',
+                view: 'View',
+                context: 'Context',
+                resources: 'Resources',
+                info: 'Info',
+                notes: 'Notes',
+            },
+        },
     });
     config.skin =
         typeof config.skin === 'string'
@@ -90,7 +89,7 @@ module.exports = function (options) {
     config.information = (
         config.information || [
             {
-                label: labels.builtOn,
+                label: config.labels.builtOn,
                 value: new Date(),
                 type: 'time',
                 format: (value) => {
