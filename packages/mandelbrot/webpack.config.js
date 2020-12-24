fc1b43355403c3d1dab4a2da820601d61674bc46
Mihkel Eidast <mihkeleidast@gmail.com>
2020-12-24 20:11:24 +0200

    use system font stacks

--- packages/mandelbrot/webpack.config.js
+++ packages/mandelbrot/webpack.config.js
@@ -60,17 +60,6 @@ module.exports = {
                 },
             },
             {
-                test: /\.(woff|woff2)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'fonts/[name].[ext]',
-                        },
-                    },
-                ],
-            },
-            {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
                     {
