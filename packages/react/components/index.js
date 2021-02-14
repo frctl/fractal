314524a6e3a29911eb9dfd172b61d2ce48692e6b
Mihkel Eidast <mihkeleidast@gmail.com>
2021-02-14 12:15:09 +0200

    export utility components from a separate index
    Using commonjs exports and exporting them from the main index means that if a user is importing the components into a bundler, the bundler will also try to bundle the main adapter code, which is not meant for bundling and will error out.

---
+++ packages/react/components/index.js
@@ -0,0 +1,7 @@
+const PathProvider = require('./path-provider');
+
+module.exports = {
+    PathProvider: PathProvider,
+    PathContext: PathProvider.PathContext,
+    usePath: PathProvider.usePath,
+};
