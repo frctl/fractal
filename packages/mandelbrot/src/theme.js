e108cccb718ba9da9d568df72b189dc2554c9b9b
Mihkel Eidast <mihkel@eida.st>
2022-01-17 19:55:42 +0200

    remove todo

--- packages/mandelbrot/src/theme.js
+++ packages/mandelbrot/src/theme.js
@@ -8,7 +8,6 @@ const packageJSON = require('../package.json');
 module.exports = function (options) {
     const config = _.defaultsDeep(_.clone(options || {}), {
         skin: 'default',
-        // TODO: needs documenting: 'default' or 'split'
         navigation: 'default',
         rtl: false,
         lang: 'en',
