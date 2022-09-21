431229bf79359599d4d2809709aa25a2ddf295f0
Joy Heron <joy.heron@innoq.com>
2022-09-21 10:16:22 +0200

    Readd Github Actions script to activate CI tests
    This also removes the `node-fetch` dependency in the examples.
    This has the effect that the `async` config for the tests doesn't
    really retrieve the data asynchronously anymore, but having an
    extra `devDependency` wasn't working with the CI, so it's not worth
    the extra effort right now.

--- example/components/context/async/async.config.js
+++ example/components/context/async/async.config.js
@@ -1,9 +1,17 @@
-const fetch = require('node-fetch');
-
-const response = fetch('https://xkcd.com/149/info.0.json').then((response) => response.json());
-
 module.exports = {
     context: {
-        xkcd: response,
+        xkcd: {
+            month: "8",
+            num: 149,
+            link: "",
+            year: "2006",
+            news: "",
+            safe_title: "Sandwich",
+            transcript: "[[ A man is sitting on a couch, talking to another man.  They are both stick figures. ]]\nFirst man:  Make me a sandwich.\nSecond man:  What?  Make it yourself.\nFirst man:  Sudo make me a sandwich.\nSecond man:  Okay",
+            alt: "Proper User Policy apparently means Simon Says.",
+            img: "https://imgs.xkcd.com/comics/sandwich.png",
+            title: "Sandwich",
+            day: "28"
+        }
     },
 };
